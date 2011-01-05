a885392bd7aa07f76cae05a9a2e77ab24de2c7f9
baluneboy <baluneboy@ab604922-b16d-4620-a341-c5f183439689>
2011-01-05 00:51:33 +0000

    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@60 ab604922-b16d-4620-a341-c5f183439689

---
+++ csharp_sandbox/clipboardFiles/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Clipboard Files")]
+[assembly: AssemblyDescription("My description goes here for clipboarding files.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("BIOKEN")]
+[assembly: AssemblyProduct("ClipboardFileGrabber")]
+[assembly: AssemblyCopyright("Copyright © BIOKEN 2011")]
+[assembly: AssemblyTrademark("Copyright © BIOKEN 2011")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("804c239e-fd21-4b07-904f-ea77f308e902")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
