322b76384ae77cacc5c6dfcdb72004f2a3abe7e0
baluneboy <baluneboy@ab604922-b16d-4620-a341-c5f183439689>
2011-01-05 00:51:33 +0000

    git-svn-id: file:///Users/ken/Repository/biomedken/trunk/csharp_sandbox@60 ab604922-b16d-4620-a341-c5f183439689

---
+++ clipboardFiles/IniFileIO.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace clipboardFiles
+{
+    class IniFileIO
+    {
+        [DllImport("kernel32.dll", EntryPoint = "GetPrivateProfileString")]
+        private static extern int GetPrivateProfileString(string lpAppName, string lpKeyName, string lpDefault, StringBuilder lpReturnedString, int nSize, string lpFileName);
+        [DllImport("kernel32.dll", EntryPoint = "WritePrivateProfileString")]
+        private static extern bool WritePrivateProfileString(string lpAppName, string lpKeyName, string lpString, string lpFileName);
+
+        static void ThisWasDemoMain(string[] args)
+        {
+            string val;
+            val = GetIniValue("B", "Key1", "c:\\temp\\initest.ini");
+            Console.WriteLine(val);
+            WriteIniValue("B", "Key1", "New Value", "c:\\temp\\initest.ini");
+
+            val = GetIniValue("D", "Key1", "c:\\temp\\initest.ini");
+            Console.WriteLine(val);
+            WriteIniValue("D", "Key1", "Value1", "c:\\temp\\initest.ini");
+        }
+
+        public static string GetIniValue(string section, string key, string filename)
+        {
+            int chars = 256;
+            StringBuilder buffer = new StringBuilder(chars);
+            string sDefault = "";
+            if (GetPrivateProfileString(section, key, sDefault,
+              buffer, chars, filename) != 0)
+            {
+                return buffer.ToString();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static bool WriteIniValue(string section, string key, string value, string filename)
+        {
+            return WritePrivateProfileString(section, key, value, filename);
+        }
+    }
+}
+
+
+
+
+
+
