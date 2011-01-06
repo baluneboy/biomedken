74b25825abb60d2496eb1dc85a339b4d1509dfbe
baluneboy <baluneboy@ab604922-b16d-4620-a341-c5f183439689>
2011-01-06 13:10:09 +0000

    Added some code toward reading Excel config file.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@65 ab604922-b16d-4620-a341-c5f183439689

--- csharp_sandbox/ExcelAutomationClient/Form1.Designer.cs
+++ csharp_sandbox/ExcelAutomationClient/Form1.Designer.cs
@@ -28,25 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonCreateDummyData = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // buttonCreateDummyData
             // 
-            this.button1.Location = new System.Drawing.Point(60, 58);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonCreateDummyData.Location = new System.Drawing.Point(31, 58);
+            this.buttonCreateDummyData.Name = "buttonCreateDummyData";
+            this.buttonCreateDummyData.Size = new System.Drawing.Size(132, 23);
+            this.buttonCreateDummyData.TabIndex = 0;
+            this.buttonCreateDummyData.Text = "CreateDummyData";
+            this.buttonCreateDummyData.UseVisualStyleBackColor = true;
+            this.buttonCreateDummyData.Click += new System.EventHandler(this.buttonCreateDummyData_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(31, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ReadExistingData";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(194, 138);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(195, 136);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonCreateDummyData);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -55,7 +67,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonCreateDummyData;
+        private System.Windows.Forms.Button button2;
     }
 }
 
