3eb6df6ae3bd8f88596e0de97adb4d4ec955c247
baluneboy <baluneboy@ab604922-b16d-4620-a341-c5f183439689>
2011-01-13 12:16:29 +0000

    Reorganized folder/file structure.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@85 ab604922-b16d-4620-a341-c5f183439689

--- csharp_sandbox/ConsoleTreeWalker/Program.cs
+++ csharp_sandbox/ConsoleTreeWalker/Program.cs
@@ -10,29 +10,29 @@ namespace ConsoleApplication1
     public class RecursiveFileSearch
     {
         static System.Collections.Specialized.StringCollection log = new System.Collections.Specialized.StringCollection();
-
+        
         static void Main()
         {
+            // Create the regular expression to find "c:\temp\dupstination\t*o".
+            System.Text.RegularExpressions.Regex patternDir =
+                new System.Text.RegularExpressions.Regex(@"^c:\\temp\\dupstination\\t[^\\]*o$");
+
             // Start with drives if you have to search the entire computer.
             DirectoryInfo diRootDir = new DirectoryInfo(@"c:\temp\dupstination");
-            WalkDirectoryTree(diRootDir);
+            WalkDirectoryTree(diRootDir,patternDir);
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key");
             Console.ReadKey();
         }
 
-        static void WalkDirectoryTree(System.IO.DirectoryInfo root)
+        static void WalkDirectoryTree(System.IO.DirectoryInfo root, System.Text.RegularExpressions.Regex patternDir)
         {
             System.IO.FileInfo[] files = null;
             System.IO.DirectoryInfo[] subDirs = null;
 
             // if this directory does not match pattern, then short-circuit outta here
             string strDir = root.FullName;
-            if (strDir.Equals(@"c:\temp\dupstination\two"))
-                Console.WriteLine("   MATCH dir  " + strDir);
-            else
-                Console.WriteLine("NOTMATCH dir  " + strDir);
 
             // First, process all the files directly under this folder
             try
@@ -56,14 +56,23 @@ namespace ConsoleApplication1
 
             if (files != null)
             {
-                foreach (System.IO.FileInfo fi in files)
+                // if this directory matches, then dip down deal with files
+                //if (strDir.Equals(@"c:\temp\dupstination\two"))
+                if ( patternDir.IsMatch(strDir) )
                 {
-                    // In this example, we only access the existing FileInfo object. If we
-                    // want to open, delete or modify the file, then
-                    // a try-catch block is required here to handle the case
-                    // where the file has been deleted since the call to TraverseTree().
-                    Console.WriteLine("file  " + fi.FullName);
+                    //Console.WriteLine("   MATCH dir  " + strDir);
+                    foreach (System.IO.FileInfo fi in files)
+                    {
+                        // In this example, we only access the existing FileInfo object. If we
+                        // want to open, delete or modify the file, then
+                        // a try-catch block is required here to handle the case
+                        // where the file has been deleted since the call to TraverseTree().
+                        if (patternDir.IsMatch(fi.Directory.FullName))
+                            Console.WriteLine("file  " + fi.FullName);
+                    }
                 }
+                //else
+                //    Console.WriteLine("NOTMATCH dir  " + strDir);
 
                 // Now find all the subdirectories under this directory.
                 subDirs = root.GetDirectories();
@@ -71,7 +80,7 @@ namespace ConsoleApplication1
                 foreach (System.IO.DirectoryInfo dirInfo in subDirs)
                 {
                     // Resursive call for each subdirectory.
-                    WalkDirectoryTree(dirInfo);
+                    WalkDirectoryTree(dirInfo,patternDir);
                 }
             }
         }
