b2c7d50fee5e36ad9cf407a77c32f302f0b23511
baluneboy <baluneboy@ab604922-b16d-4620-a341-c5f183439689>
2011-01-14 13:50:52 +0000

    Added ClassLibraryDemo.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@91 ab604922-b16d-4620-a341-c5f183439689

---
+++ csharp_sandbox/ClassLibraryDemo/StupidReverser.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibraryDemo
+{
+    //public class StringReverser
+    //{
+    //    private string str;
+    //    public string pub;
+
+    //    public string StringReverser(string s)
+    //    {
+    //        str = s + "suffix";
+    //        return str;
+    //    }
+
+    //    private string Reverse2(string x)
+    //    {
+    //        char[] charArray = new char[x.Length];
+    //        int len = x.Length - 1;
+    //        for (int i = 0; i <= len; i++)
+    //            charArray[i] = x[len - i];
+    //        return new string(charArray);
+    //    }
+    //}
+
+    public class StringReverser
+    {
+        private string withSuffix;
+        private int oldLength = 11;
+
+        public int NewLength
+        {
+            get { return 11+3; }
+            set
+            {
+                if (value <= 65 && value >= 18)
+                {
+                    oldLength = value;
+                }
+                else
+                    oldLength = 18;
+            }
+        }
+        public string RevStr
+        {
+            get { return withSuffix; }
+            set { withSuffix = value + "suffix"; }
+        }
+    }
+
+    public class TestClass
+    {
+        int myVar;
+
+        // Default constructor
+        public int TestClass(int x)
+        {
+            myVar = x;
+            return myVar;
+        }
+    }
+
+}
