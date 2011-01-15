1bc4b7d14a1cee97cf04aa0f902abe2180bef2a2
baluneboy <baluneboy@ab604922-b16d-4620-a341-c5f183439689>
2011-01-15 15:30:43 +0000

    Add ConAppMyGlob
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk/csharp_sandbox@99 ab604922-b16d-4620-a341-c5f183439689

---
+++ ConAppMyGlob/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ClassLibraryFileGlobber;
+
+namespace ConAppMyGlob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string argsZero = @"C:\temp\dupstination\t*o\tra*.txt";
+            // Glob the pattern
+            //FileGlobber fg = new FileGlobber(args[0]);
+            FileGlobber fg = new FileGlobber(argsZero);
+            //System.Console.WriteLine("GlobPattern is " + fg.GlobPattern);
+            //System.Console.WriteLine("AreValid " + fg.GlobParts.AreValid + " 4 " + fg.GlobParts.BasePath);
+            //System.Console.WriteLine("DirPattern is " + fg.GlobParts.DirPattern);
+            //System.Console.WriteLine("FilePattern is " + fg.GlobParts.FilePattern);
+            
+            foreach (string fileName in fg.FileQueue)
+            {
+                System.Console.WriteLine("FOUND FILE: " + fileName);
+            }
+
+        }
+    }
+}
