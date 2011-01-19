6c3bd9f1be85ee53bff18b1f9d00ef7fcc1f9a0d
baluneboy <baluneboy@ab604922-b16d-4620-a341-c5f183439689>
2011-01-19 00:37:03 +0000

    Added WinAppMyGlob.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk/csharp_sandbox@105 ab604922-b16d-4620-a341-c5f183439689

---
+++ WinAppMyGlob/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WinAppMyGlob
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGlob = new System.Windows.Forms.Button();
+            this.clbFiles = new System.Windows.Forms.CheckedListBox();
+            this.txtGlobPattern = new System.Windows.Forms.TextBox();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.btnSelectAll = new System.Windows.Forms.Button();
+            this.btnUnselectAll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGlob
+            // 
+            this.btnGlob.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGlob.Location = new System.Drawing.Point(12, 17);
+            this.btnGlob.Name = "btnGlob";
+            this.btnGlob.Size = new System.Drawing.Size(57, 32);
+            this.btnGlob.TabIndex = 0;
+            this.btnGlob.Text = "Glob";
+            this.btnGlob.UseCompatibleTextRendering = true;
+            this.btnGlob.UseVisualStyleBackColor = true;
+            this.btnGlob.Click += new System.EventHandler(this.btnGlob_Click);
+            // 
+            // clbFiles
+            // 
+            this.clbFiles.Font = new System.Drawing.Font("MS Reference Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clbFiles.FormattingEnabled = true;
+            this.clbFiles.Location = new System.Drawing.Point(142, 57);
+            this.clbFiles.Name = "clbFiles";
+            this.clbFiles.Size = new System.Drawing.Size(1103, 460);
+            this.clbFiles.TabIndex = 1;
+            this.clbFiles.UseCompatibleTextRendering = true;
+            this.clbFiles.DataSourceChanged += new System.EventHandler(this.clbFiles_DataSourceChanged);
+            this.clbFiles.SelectedIndexChanged += new System.EventHandler(this.clbFiles_SelectedIndexChanged);
+            // 
+            // txtGlobPattern
+            // 
+            this.txtGlobPattern.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.txtGlobPattern.Location = new System.Drawing.Point(75, 19);
+            this.txtGlobPattern.Name = "txtGlobPattern";
+            this.txtGlobPattern.Size = new System.Drawing.Size(1170, 22);
+            this.txtGlobPattern.TabIndex = 2;
+            this.txtGlobPattern.Text = "c:\\temp\\dupstination\\t*o\\tra*.txt";
+            this.txtGlobPattern.TextChanged += new System.EventHandler(this.txtGlobPattern_TextChanged);
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcess.Location = new System.Drawing.Point(12, 123);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(124, 33);
+            this.btnProcess.TabIndex = 3;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseCompatibleTextRendering = true;
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // btnSelectAll
+            // 
+            this.btnSelectAll.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelectAll.Location = new System.Drawing.Point(12, 55);
+            this.btnSelectAll.Name = "btnSelectAll";
+            this.btnSelectAll.Size = new System.Drawing.Size(124, 28);
+            this.btnSelectAll.TabIndex = 4;
+            this.btnSelectAll.Text = "select all";
+            this.btnSelectAll.UseCompatibleTextRendering = true;
+            this.btnSelectAll.UseVisualStyleBackColor = true;
+            this.btnSelectAll.Click += new System.EventHandler(this.btnSelectAll_Click);
+            // 
+            // btnUnselectAll
+            // 
+            this.btnUnselectAll.Font = new System.Drawing.Font("MS Reference Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUnselectAll.Location = new System.Drawing.Point(12, 89);
+            this.btnUnselectAll.Name = "btnUnselectAll";
+            this.btnUnselectAll.Size = new System.Drawing.Size(124, 28);
+            this.btnUnselectAll.TabIndex = 5;
+            this.btnUnselectAll.Text = "unselect all";
+            this.btnUnselectAll.UseCompatibleTextRendering = true;
+            this.btnUnselectAll.UseVisualStyleBackColor = true;
+            this.btnUnselectAll.Click += new System.EventHandler(this.btnUnselectAll_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1289, 565);
+            this.Controls.Add(this.btnUnselectAll);
+            this.Controls.Add(this.btnSelectAll);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.txtGlobPattern);
+            this.Controls.Add(this.clbFiles);
+            this.Controls.Add(this.btnGlob);
+            this.Location = new System.Drawing.Point(0, 11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGlob;
+        private System.Windows.Forms.CheckedListBox clbFiles;
+        private System.Windows.Forms.TextBox txtGlobPattern;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Button btnSelectAll;
+        private System.Windows.Forms.Button btnUnselectAll;
+    }
+}
+
---
+++ WinAppMyGlob/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+using WinAppMyGlob.Properties;
+using ClassLibraryFileGlobber;
+
+namespace WinAppMyGlob
+{
+    public partial class Form1 : Form
+    {
+        string[] myFiles = {
+                            "Nothing to see here yet, do following steps first:",
+                            "1. Set glob pattern in text box above, then...",
+                            "2. Press the 'Glob' button for matching files." };
+        
+        DataTable dtConfig = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.clbFiles.DataSource = myFiles;
+
+            string ConfigFile = System.IO.Path.Combine(Properties.Settings.Default.ConfigPath,
+                Properties.Settings.Default.ConfigFile);
+            OleDbConnection dbConnection = new OleDbConnection(
+                @"Provider=Microsoft.ACE.OLEDB.12.0;"
+                + @"Data Source=" + ConfigFile + ";"
+                + @"Extended Properties=""Excel 12.0;HDR=Yes;""");
+            dbConnection.Open();
+            try
+            {
+                OleDbDataAdapter dbAdapter = new OleDbDataAdapter(
+                    "SELECT * FROM [config$]", dbConnection);
+                dbAdapter.Fill(dtConfig);
+            }
+            finally
+            {
+                dbConnection.Close();
+            }
+
+        }
+
+        private void txtGlobPattern_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGlob_Click(object sender, EventArgs e)
+        {
+            // message
+            clbFiles.DataSource = new string[] { "Now gathering files that match..." };
+
+            // get glob pattern
+            string globPattern = txtGlobPattern.Text;
+            FileGlobber fg = new FileGlobber(globPattern);
+
+            // clear & populate listbox
+            clbFiles.DataSource = null;
+            clbFiles.DataSource = fg.MatchingFiles.ToList();
+
+        }
+
+        private void clbFiles_DataSourceChanged(object sender, EventArgs e)
+        {
+            /*
+			*The following code is required to remove existing items from the
+            *Items collection when the DataSource is set to null.
+			*/
+
+            ListBox ctlLIST = (ListBox)sender;
+            if (ctlLIST.DataSource == null)
+                ctlLIST.Items.Clear();
+        }
+
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            //// Determine if there are any items checked.
+            //if (clbFiles.CheckedItems.Count != 0)
+            //{
+            //    // If so, loop through all checked items and print results.
+            //    string s = "";
+            //    for (int x = 0; x <= clbFiles.CheckedItems.Count - 1; x++)
+            //    {
+            //        s = s + "Checked Item " + (x + 1).ToString() + " = " + clbFiles.CheckedItems[x].ToString() + "\n";
+            //    }
+            //    MessageBox.Show(s);
+            //}
+
+            // Iterate over config's data table contents to glob & process
+            List<string> filelist = new List<string>();
+            string globpat;
+            string s = "";
+            foreach (DataRow row in dtConfig.Rows)
+            {
+                string subject = row["Subject"].ToString();
+                string session = row["Session"].ToString();
+                string task = row["Task"].ToString();
+
+                // build glob pattern to anatomical image
+                globpat = row["Basepath"].ToString() + @"\" +
+                          subject + @"\" +
+                          session + @"\" +
+                          "study_*" + @"\" + "results" + @"\" +
+                          task + @"\" +
+                          "w2*WHOLEHEAD*.hdr";
+
+                // glob for matching files
+                FileGlobber fg = new FileGlobber(globpat);
+
+                // if exactly one anat header file found, then check; otherwise uncheck
+                if (fg.MatchCount == 1)
+                {
+                    // FIXME better method for add single FileInfo item to list!?
+                    foreach (var fi in fg.MatchingFiles)
+                        filelist.Add(fi.FullName);
+                }
+                else
+                    s = s + "No anat file for " + subject + " " + session + " " + task;
+
+            }
+            MessageBox.Show(s);
+            clbFiles.DataSource = filelist;
+            for (int i = 0; i < clbFiles.Items.Count; i++)
+                clbFiles.SetItemChecked(i, true);
+        }
+
+        private void clbFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSelectAll_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < clbFiles.Items.Count; i++)
+                clbFiles.SetItemChecked(i, true);
+        }
+
+        private void btnUnselectAll_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < clbFiles.Items.Count; i++)
+                clbFiles.SetItemChecked(i, false);
+        }
+
+    }
+}
