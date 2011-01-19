b07518571f3b9f2d78b10bd419f2a5e0b37ae197
baluneboy <baluneboy@ab604922-b16d-4620-a341-c5f183439689>
2011-01-19 23:50:09 +0000

    Hooked in calls to MRIcroN!
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@108 ab604922-b16d-4620-a341-c5f183439689

--- csharp_sandbox/WinAppMyGlob/Form1.cs
+++ csharp_sandbox/WinAppMyGlob/Form1.cs
@@ -20,8 +20,10 @@ namespace WinAppMyGlob
                             "Nothing to see here yet, do following steps first:",
                             "1. Set glob pattern in text box above, then...",
                             "2. Press the 'Glob' button for matching files." };
-        
         DataTable dtConfig = new DataTable();
+
+        private List<string> anatomylist = new List<string>();
+        private List<string> overlaylist = new List<string>();
 
         public Form1()
         {
@@ -92,6 +94,8 @@ namespace WinAppMyGlob
             List<string> filelist = new List<string>();
             List<Boolean> blnlist = new List<Boolean>();
             string globpat;
+            string str = "unknown status";
+            Boolean bln = false;
             foreach (DataRow row in dtConfig.Rows)
             {
                 string subject = row["Subject"].ToString();
@@ -116,19 +120,30 @@ namespace WinAppMyGlob
                     // FIXME better method for add single FileInfo item to list!?
                     foreach (var fi in fg.MatchingFiles)
                     {
-                        filelist.Add(fi.FullName);
-                        blnlist.Add(true);
-                        //if (File.Exists(fi.DirectoryName + overlay))
-                        //    MessageBox.Show(subject + " " + session + " " + task + " overlay exists.");
-                        //else
-                        //    MessageBox.Show(subject + " " + session + " " + task + " OVERLAY DOES NOT EXIST!");
+                        if (File.Exists(fi.DirectoryName + @"\" + overlay))
+                        {
+                            //MessageBox.Show(fi.DirectoryName + @"\" + overlay + " exists.");
+                            //str = fi.FullName;
+                            str = subject + " " + session + " " + task + " found both anatomy & overlay files.";
+                            bln = true;
+                            anatomylist.Add(fi.FullName);
+                            overlaylist.Add(fi.DirectoryName + @"\" + overlay);
+                        }
+                        else
+                        {
+                            //MessageBox.Show(fi.DirectoryName + @"\" + overlay + " does NOT exist!");
+                            str = subject + " " + session + " " + task + " found no overlay file.";
+                            bln = false;
+                        }
                     }
                 }
                 else
                 {
-                    filelist.Add("No anat file for " + subject + " " + session + " " + task);
-                    blnlist.Add(false);
+                    str = subject + " " + session + " " + task + " found no anatomy file.";
+                    bln = false;
                 }
+                filelist.Add(str);
+                blnlist.Add(bln);
             }
             clbFiles.DataSource = filelist;
             for (int i = 0; i < clbFiles.Items.Count; i++)
@@ -158,11 +173,26 @@ namespace WinAppMyGlob
 
         private void btnProcess_Click(object sender, EventArgs e)
         {
-            Process p = new Process();
-            p.StartInfo.FileName = @"C:\Program Files\mricron\MRIcroN.exe";
-            p.StartInfo.Arguments = @"Y:\adat\c1316plas\pre\study_20060608\results\wrist\w20060608_132633WHOLEHEAD1MMs004a001.hdr";
-            p.Start();
+            for (int i = 0; i < anatomylist.Count; i++)
+            {
+                string anat = anatomylist[i];
+                string over = @" -c grayscale -o " + overlaylist[i];
+                //MessageBox.Show(anatomylist[i] + "\n" + overlaylist[i]);
+                Process p = new Process();
+                p.StartInfo.FileName = @"C:\Program Files\mricron\MRIcroN.exe";
+                //p.StartInfo.Arguments = @"N:\data\adat\c1316plas\pre\study_20060608\results\shoulder\w20060608_132633WHOLEHEAD1MMs004a001.hdr -c grayscale -o N:\data\adat\c1316plas\pre\study_20060608\results\shoulder\roi_adat\masked_roi98_mniwholebrain_fromspm_wroi99_wholecube_both_p-overlay_adathreshold_remap.hdr";
+                p.StartInfo.Arguments = anat + over;
+                p.Start();
+            }
         }
 
     }
 }
+
+/* 
+ * TODO
+ * - after process button starts mricron(s), close this form gracefully
+ * - fully integrate with excel
+ * - implement LUT (via Key/Val pairs) for excel, like: subject, session, task, overlay
+ * 
+ */
