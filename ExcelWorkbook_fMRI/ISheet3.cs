891593e1740e533a07b55c1a5007bf75fbd15597
baluneboy <baluneboy@ab604922-b16d-4620-a341-c5f183439689>
2011-01-25 02:36:42 +0000

    Add code for VSTO with fMRI viewer.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk/csharp_sandbox@132 ab604922-b16d-4620-a341-c5f183439689

---
+++ ExcelWorkbook_fMRI/ISheet3.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ExcelWorkbook_fMRI
+{
+    [System.Runtime.InteropServices.ComVisible(true)]
+    public interface ISheet3
+    {
+        void CreateVstoNamedRange(Microsoft.Office.Interop.Excel.Range range, string name);
+        string CreateVstoPath(string name);
+    }
+
+}
