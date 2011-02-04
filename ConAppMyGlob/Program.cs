1a4af860afbdb26cb00755e5ae82d655df28bbf6
baluneboy <baluneboy@ab604922-b16d-4620-a341-c5f183439689>
2011-02-04 09:28:00 +0000

    Reused Program.cs in ConAppMyGlob
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk/csharp_sandbox@160 ab604922-b16d-4620-a341-c5f183439689

--- ConAppMyGlob/Program.cs
+++ ConAppMyGlob/Program.cs
@@ -20,17 +20,30 @@ namespace ConAppMyGlob
             FileGlobberFmriAnat.print();
             ((FileGlobber)FileGlobberFmriAnat).print();
             Console.ReadLine();
+
 #elif (DEMO && !MYTEST)
+
             Console.WriteLine("DEMO is defined");
             FileGlobberFmriAnat fga = new FileGlobberFmriAnat(args[0]);
-
             if (fga.IsValid)
             {
-                Console.WriteLine("FA " + fga.FileAnat);
-                Console.WriteLine("PA " + fga.PathAnat);
+                //Console.WriteLine("FA " + fga.FileAnat);
+                System.IO.Directory.CreateDirectory(fga.PathAnat.Replace(@"y:\adat", @"t:\previousResults") + @"\..");
+                Console.WriteLine("created " + fga.PathAnat.Replace(@"y:\adat", @"t:\previousResults") + @"\..");
             }
             else
                 Console.WriteLine("not valid bc MatchCount is " + fga.MatchCount);
+
+            FileGlobberFmriAnat fgaW = new FileGlobberFmriAnat(args[0].Replace("shoulder","wrist"));
+            if (fgaW.IsValid)
+            {
+                //Console.WriteLine("FA " + fga.FileAnat);
+                System.IO.Directory.CreateDirectory(fgaW.PathAnat.Replace(@"y:\adat", @"t:\previousResults") + @"\..");
+                Console.WriteLine("created " + fgaW.PathAnat.Replace(@"y:\adat", @"t:\previousResults") + @"\..");
+            }
+            else
+                Console.WriteLine("not valid bc MatchCount is " + fga.MatchCount);
+
 #elif (DEMO && MYTEST)
             Console.WriteLine("DEMO and MYTEST are defined");
 #else
