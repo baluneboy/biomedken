f423342c827f4c0223cc79046fbfca7998cbe286
baluneboy <baluneboy@ab604922-b16d-4620-a341-c5f183439689>
2011-02-05 18:38:55 +0000

    Added named range controls.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@167 ab604922-b16d-4620-a341-c5f183439689

--- csharp_sandbox/ExcelWorkbook_fMRI/Sheet2.cs
+++ csharp_sandbox/ExcelWorkbook_fMRI/Sheet2.cs
@@ -15,30 +15,70 @@ namespace ExcelWorkbook_fMRI
 {
     public partial class Sheet2
     {
+        Microsoft.Office.Tools.Excel.NamedRange basePathRange;
+        Microsoft.Office.Tools.Excel.NamedRange mricronExeRange;
+
         private void Sheet2_Startup(object sender, System.EventArgs e)
         {
-            this.Change += new Excel.DocEvents_ChangeEventHandler(Sheet2_Change);
+            //this.Change += new Excel.DocEvents_ChangeEventHandler(Sheet2_Change);
+            NotifyChanges();
+        }
+
+        private void NotifyChanges()
+        {
+            // TODO verify this is the range by checking if Offset(0,-1) is "basePath"
+            basePathRange = this.Controls.AddNamedRange(
+                this.Range["B1"], "basePathRange");
+            basePathRange.Change += new Microsoft.Office.Interop.Excel.
+                DocEvents_ChangeEventHandler(basePathRange_Change);
+
+            // TODO verify this is the range by checking if Offset(0,-1) is "MRIcroNexe"
+            mricronExeRange = this.Controls.AddNamedRange(
+                this.Range["B2"], "mricronExeRange");
+            mricronExeRange.Change += new Microsoft.Office.Interop.Excel.
+                DocEvents_ChangeEventHandler(mricronExeRange_Change);
+        }
+
+        void mricronExeRange_Change(Excel.Range Target)
+        {
+            string cellAddress = Target.get_Address(missing, missing,
+                Microsoft.Office.Interop.Excel.XlReferenceStyle.xlA1,
+                missing, missing);
+            if (!File.Exists(Target.Value2))
+                MessageBox.Show("MRIcroNexe in cell " + cellAddress + " changed to non-existing file!");
+            // TODO offer file dialog
+        }
+
+        void basePathRange_Change(Excel.Range Target)
+        {
+            string cellAddress = Target.get_Address(missing, missing,
+                Microsoft.Office.Interop.Excel.XlReferenceStyle.xlA1,
+                missing, missing);
+            if (!Directory.Exists(Target.Value2))
+                MessageBox.Show("basePath in cell " + cellAddress + " changed to non-existing path!");
+            // TODO offer directory seletion dialog (defaults to pwd)
         }
 
         private void Sheet2_Shutdown(object sender, System.EventArgs e)
         {
         }
 
+        public void MaybeComplain(bool blnComplain, string msgComplaint)
+        {
+            if (blnComplain)
+                MessageBox.Show(msgComplaint);
+        }
+
         void Sheet2_Change(Excel.Range Target)
         {
-            if (Target.Row.Equals(2))
-                if (Target.Column.Equals(1)) // basePath
-                    if (!Directory.Exists(Target.Value2))
-                    {
-                        MessageBox.Show("basePath in " + this.Name + " row " + Target.Row + " " + ", column " + Target.Column + " changed to path I could not find.", "PROBLEM");
-                        //Application.Undo(); // TODO fix endless loop when initial value is bad here
-                    }
-                if (Target.Column.Equals(2)) // MRIcroNexe
-                    if (!File.Exists(Target.Value2))
-                    {
-                        MessageBox.Show("MRIcroNexe in " + this.Name + " row " + Target.Row + " " + ", column " + Target.Column + " changed to path/file I could not find.", "PROBLEM");
-                        //Application.Undo(); // TODO fix endless loop when initial value is bad here
-                    }
+            //// TODO verify that column to left of these Targets are labels for the dir/file of interest
+            //if (Target.Column.Equals(2))
+            //{
+            //    if (Target.Row.Equals(1)) // basePath
+            //        MaybeComplain(!Directory.Exists(Target.Value2), "basePath just entered does not exist");
+            //    if (Target.Row.Equals(2)) // MRIcroNexe
+            //        MaybeComplain(!File.Exists(Target.Value2), "MRIcroNexe file just entered does not exist");
+            //}
         }
 
         #region VSTO Designer generated code
