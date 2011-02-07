dcf15f22cfe2d402c5d393ef8c1b900e1c325a68
baluneboy <baluneboy@ab604922-b16d-4620-a341-c5f183439689>
2011-02-07 12:20:38 +0000

    Added IThisWorkbook.cs in ExcelWorkbook_fMRI.csproj but still cannot get access to ThisWorkbook.cs (YET).
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk/csharp_sandbox@173 ab604922-b16d-4620-a341-c5f183439689

---
+++ ExcelWorkbook_fMRI/IThisWorkbook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcelWorkbook_fMRI
+{
+    [System.Runtime.InteropServices.ComVisible(true)]
+    public interface IThisWorkbook
+    {
+        void InitializeIndicators();
+    }
+}
