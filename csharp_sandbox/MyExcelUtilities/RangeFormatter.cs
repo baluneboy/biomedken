863e95612940141dc75d7f1b240afb75cbf15707
baluneboy <baluneboy@ab604922-b16d-4620-a341-c5f183439689>
2011-02-09 23:53:43 +0000

    Righted the RangeFormatter.cs situation (for now).
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@180 ab604922-b16d-4620-a341-c5f183439689

---
+++ csharp_sandbox/MyExcelUtilities/RangeFormatter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace MyExcelUtilities
+{
+    public class RangeFormatter
+    {
+        protected Excel.Range rng;
+
+        // default constructor
+        public RangeFormatter() { }
+
+        // instance constructor
+        public RangeFormatter(Excel.Range r)
+        {
+            rng = r;
+        }
+
+        // method to format as "ready"
+        public void Ready()
+        {
+            rng.Interior.ColorIndex = 4;
+            rng.Font.Bold = true;
+        }
+
+        // method to format as "good"
+        public void Good()
+        {
+            rng.Font.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.Green);
+        }
+
+        // method to format as "bad"
+        public void Bad()
+        {
+            rng.Font.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.Red);
+        }
+    }
+}
