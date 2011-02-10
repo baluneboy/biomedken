859bd86dd5121f7030f5f3a2c288b1e7f1782d43
baluneboy <baluneboy@ab604922-b16d-4620-a341-c5f183439689>
2011-02-10 12:44:07 +0000

    renamed
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@182 ab604922-b16d-4620-a341-c5f183439689

---
+++ csharp_sandbox/FileGlobberClassLibrary/ClassInheritance.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ClassLibraryFileGlobber;
+
+namespace ClassLibraryFileGlobber
+{
+
+    public class ParentDemo
+    {
+        string ParentDemoString;
+        public ParentDemo()
+        {
+            Console.WriteLine("ParentDemo Constructor.");
+        }
+        public ParentDemo(string myString)
+        {
+            ParentDemoString = myString;
+            Console.WriteLine(ParentDemoString);
+        }
+        public void print()
+        {
+            Console.WriteLine("I'm a ParentDemo Class.");
+        }
+    }
+
+    public class ChildDemo : ParentDemo
+    {
+        public ChildDemo()
+            : base("From Derived")
+        {
+            Console.WriteLine("ChildDemo Constructor.");
+        }
+        public new void print()
+        {
+            base.print();
+            Console.WriteLine("I'm a ChildDemo Class.");
+        }
+        public static void NotMain()
+        {
+            ChildDemo ChildDemo = new ChildDemo();
+            ChildDemo.print();
+            ((ParentDemo)ChildDemo).print();
+            Console.ReadLine();
+        }
+    }
+
+}
