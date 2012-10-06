318f176c4f7178e18f83ea36d441b38b6eb1a437
baluneboy <baluneboy@ab604922-b16d-4620-a341-c5f183439689>
2012-10-06 17:39:23 +0000

    Did svn delete on several obsolete directories.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@243 ab604922-b16d-4620-a341-c5f183439689

--- dalydemo/doc_hggroup.m
+++
@@ -1,8 +0,0 @@
-function hg = doc_hggroup
-hg = hggroup('ButtonDownFcn',@set_lines);
-h  = line(randn(5),randn(5),'HitTest','off','Parent',hg);
-
-function set_lines(cb,eventdata)
-hl = get(cb,'Children');% cb is handle of hggroup object
-lw = get(hl,'LineWidth');% get current line widths
-set(hl,{'LineWidth'},num2cell([lw{:}]+1,[5,1])')
