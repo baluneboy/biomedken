068df30b711bc6af7d74baad9aaf3808047e56ca
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2012-12-31 20:27:56 +0000

    refined recipes_fileutils.py
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@269 ab604922-b16d-4620-a341-c5f183439689

--- recipes/recipes_fileutils.py
+++ recipes/recipes_fileutils.py
@@ -15,10 +15,14 @@ def filter_filenames(dirpath, predicate):
             if predicate(abspath):
                 yield abspath
 
-if __name__ == "__main__":
-    dirpath = '/misc/jaxa'
-    filePattern = '\d{14}.\d{14}/\d{4}_\d{2}_\d{2}_\d{2}_\d{2}_\d{2}.\d{3}.\d{4}_\d{2}_\d{2}_\d{2}_\d{2}_\d{2}.\d{3}.*'
+def demo_show_matches(dirpath, filenamePattern):
+    """walk dirpath and show regex matches of filenamePattern"""
     fullFilePattern = os.path.join(dirpath, filePattern)
     print 'filter_filenames matching pattern "%s"\n================================' % fullFilePattern
     for f in filter_filenames(dirpath, re.compile(fullFilePattern).match):
-        print "%s" % f
+        print "%s" % f    
+
+if __name__ == "__main__":
+    dirpath = '/Users/ken/Desktop/music23/zmusic'
+    filePattern = '(.*)-(.*)'
+    demo_show_matches(dirpath, filePattern)
