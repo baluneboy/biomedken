136df628ceb3d0306a9bd6689138c84ac8127210
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2012-12-31 19:48:38 +0000

    started to generalize plot intervals code (in recipes)
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@268 ab604922-b16d-4620-a341-c5f183439689

--- recipes/recipes_plot_intervals.py
+++ recipes/recipes_plot_intervals.py
@@ -67,5 +67,48 @@ def showDemo():
     xLabel = ax.set_xlabel('GMT')
     plt.show()    
 
+
+def demo2():
+    import random
+    # A generator to get datetimes (every 5 minutes start on 31-Dec-2012)
+    def nextFiveMinutes():
+        dtm = datetime.datetime(2012, 12, 31, 22, 0, 0) - datetime.timedelta(minutes=5)
+        while 1:
+            dtm += datetime.timedelta(minutes=5)
+            yield (dtm)
+    
+    # Generate dummy data
+    nd = nextFiveMinutes()
+    num = 38
+    some_dates = [nd.next() for i in range(0,num)] #get 20 dates
+    y_values = [random.randint(1,100) for i in range(0,num)] # get dummy y data
+    
+    # Figure and axis objects
+    fig = plt.figure()
+    ax = fig.gca()
+    
+    # Plotting goes here ...
+    ax.plot_date(some_dates, y_values, 'b.-')
+    ax.set_xlabel('GMT (hh:mm)')
+    
+    # Set major x ticks every 15 minutes
+    ax.xaxis.set_major_locator( matplotlib.dates.MinuteLocator(byminute=[0, 30, 60]) )
+    ax.xaxis.set_minor_locator( matplotlib.dates.MinuteLocator(byminute=[15, 45]) )
+    ax.xaxis.set_major_formatter( matplotlib.dates.DateFormatter('%H:%M\n%d-%b-%Y') )
+    ax.xaxis.set_minor_formatter( matplotlib.dates.DateFormatter('%H:%M') )
+    
+    # Make tick_params more suitable to your liking...
+    plt.tick_params(axis='both', which='both', width=2, direction='out')
+    # tick_params for x-axis
+    plt.tick_params(axis='x', which='major', labelsize=12, length=8)
+    plt.tick_params(axis='x', which='minor', labelsize=12)
+    plt.tick_params(axis='x', which='minor', length=6, colors='gray')
+    # tick_params for y-axis
+    plt.tick_params(axis='y', which='both', labelsize=12)
+    plt.tick_params(right=True, labelright=True)
+    
+    plt.show()
+
 if __name__ == '__main__':
-    showDemo()
+    #showDemo()
+    demo2()
