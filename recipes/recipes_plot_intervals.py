b6b949edb5df8a587ac895c82236725504e638f1
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2013-01-03 11:41:10 +0000

    Commit recipes_plot_intervals.py after trim back (most went to pimschecker path).
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@273 ab604922-b16d-4620-a341-c5f183439689

--- recipes/recipes_plot_intervals.py
+++ recipes/recipes_plot_intervals.py
@@ -4,12 +4,6 @@ import matplotlib.pyplot as plt
 from matplotlib.dates import DateFormatter, HourLocator, MinuteLocator
 import numpy as np
 import datetime
-import sys
-
-if sys.version_info > (2, 7):
-    USETICKPARAMS = True
-else:
-    USETICKPARAMS = False
 
 def plotIntervalSet(fig, ax, yval, start, stop, color='k', lw=6):
     """ plot interval data """
@@ -69,69 +63,12 @@ def showDemo():
     ax.set_ylim([0, 5])
     ax.set_xlim([start.min()-delta, stop.max()+delta])
     xLabel = ax.set_xlabel('GMT (HH:MM)')
-    
-    if USETICKPARAMS:
-        # Make tick_params more suitable to your liking...
-        plt.tick_params(axis='both', which='both', width=2, direction='out')
-        # tick_params for x-axis
-        plt.tick_params(axis='x', which='major', labelsize=12, length=8)
-        plt.tick_params(axis='x', which='minor', labelsize=12)
-        plt.tick_params(axis='x', which='minor', length=6, colors='gray')
-        # tick_params for y-axis
-        plt.tick_params(axis='y', which='both', labelsize=12)
-        plt.tick_params(right=True, labelright=True)
-    else:
-        print 'matplotlib version prohibits use of tick_params'
-    
+        
     ax.xaxis.grid(True,'minor')
     ax.xaxis.grid(True,'major',linewidth=2)
     
     plt.savefig('/tmp/trash.pdf')
     plt.show()
     
-
-def demo2():
-    import random
-    # A generator to get datetimes (every 5 minutes start on 31-Dec-2012)
-    def nextFiveMinutes():
-        dtm = datetime.datetime(2012, 12, 31, 22, 0, 0) - datetime.timedelta(minutes=5)
-        while 1:
-            dtm += datetime.timedelta(minutes=5)
-            yield (dtm)
-    
-    # Generate dummy data
-    nd = nextFiveMinutes()
-    num = 38
-    some_dates = [nd.next() for i in range(0,num)] #get 20 dates
-    y_values = [random.randint(1,100) for i in range(0,num)] # get dummy y data
-    
-    # Figure and axis objects
-    fig = plt.figure()
-    ax = fig.gca()
-    
-    # Plotting goes here ...
-    ax.plot_date(some_dates, y_values, 'b.-')
-    ax.set_xlabel('GMT (hh:mm)')
-    
-    # Set major x ticks every so often
-    ax.xaxis.set_major_locator( MinuteLocator(byminute=[0, 30, 60]) )
-    ax.xaxis.set_minor_locator( MinuteLocator(byminute=[15, 45]) )
-    ax.xaxis.set_major_formatter( DateFormatter('%H:%M\n%d-%b-%Y') )
-    ax.xaxis.set_minor_formatter( DateFormatter('%H:%M') )
-    
-    if USETICKPARAMS:
-        # Make tick_params more suitable to your liking...
-        plt.tick_params(axis='both', which='both', width=2, direction='out')
-        # tick_params for x-axis
-        plt.tick_params(axis='x', which='major', labelsize=12, length=8)
-        plt.tick_params(axis='x', which='minor', labelsize=12)
-        plt.tick_params(axis='x', which='minor', length=6, colors='gray')
-        # tick_params for y-axis
-        plt.tick_params(axis='y', which='both', labelsize=12)
-        plt.tick_params(right=True, labelright=True)
-    
-    plt.show()
-
 if __name__ == '__main__':
     showDemo()
-    demo2()
