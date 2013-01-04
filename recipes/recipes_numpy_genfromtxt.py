ac71328b28b9deae32c8152e09d9229db522d47c
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2013-01-04 15:05:58 +0000

    Added doctest to recipes_numpy_genfromtxt.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@274 ab604922-b16d-4620-a341-c5f183439689

--- recipes/recipes_numpy_genfromtxt.py
+++ recipes/recipes_numpy_genfromtxt.py
@@ -24,11 +24,24 @@ def getDemoData():
     return caption, start, stop, state
 
 def showDemo():
+    """
+    
+    >>> showDemo()
+    a 31-Dec-2012/02:15:22 31-Dec-2012/22:15:30 OK
+    b 31-Dec-2012/23:45:33 01-Jan-2013/11:05:40 BAD
+    c 01-Jan-2013/11:25:40 01-Jan-2013/19:44:55 OK
+    
+    """
+    
     fmt = '%d-%b-%Y/%H:%M:%S'
     # Build y values from the number of start values
     caption, start, stop, state = getDemoData()
     for cap,t1,t2,cond in zip(caption,start,stop,state):
         print cap, t1.strftime(fmt), t2.strftime(fmt), cond
 
-if __name__ == '__main__':
-    showDemo()
+def testdoc():
+    import doctest
+    return doctest.testmod()
+
+if __name__ == "__main__":
+    testdoc() # pass "-v" as input arg to see verbose test output
