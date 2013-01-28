0153a3dd0136933dde851adf882ab20789f23317
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2013-01-28 13:46:54 +0000

    Added comments on better daemon implementation.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@283 ab604922-b16d-4620-a341-c5f183439689

--- recipes/recipes_daemon.py
+++ recipes/recipes_daemon.py
@@ -5,12 +5,14 @@ import time
 from daemon import runner
 
 class App():
+    
     def __init__(self):
         self.stdin_path = '/dev/null'
         self.stdout_path = '/dev/tty'
         self.stderr_path = '/dev/tty'
         self.pidfile_path =  '/tmp/foo_daemon.pid'
         self.pidfile_timeout = 5
+    
     def run(self):
         while True:
             print("Howdy World!")
@@ -19,3 +21,78 @@ class App():
 app = App()
 daemon_runner = runner.DaemonRunner(app)
 daemon_runner.do_action()
+
+
+####!/usr/bin/python
+###from daemon import Daemon
+###import sys
+###import time
+###import logging
+###
+###PIDFILE = '/var/run/yourdaemon.pid'
+###LOGFILE = '/var/log/yourdaemon.log'
+###
+#### Configure logging
+###logging.basicConfig(filename=LOGFILE,level=logging.DEBUG)
+###
+###class YourDaemon(Daemon):
+###
+###	def run(self):
+###		# Define your tasks here
+###		# Anything written in python is permitted
+###		# For example you can clean up your server logs every hour
+###
+###
+###		# Logging errors and exceptions
+###		try:
+###			pass
+###		except Exception, e:
+###			logging.exception('Human friendly error message, the exception will be captured and added to the log file automaticaly')
+###
+###		while True:
+###			# The daemon will repeat your tasks according to this variable
+###			# it's in second so 60 is 1 minute, 3600 is 1 hour, etc. 
+###			time.sleep(60)
+###
+###if __name__ == "__main__":
+###
+###	daemon = YourDaemon(PIDFILE)
+###
+###	if len(sys.argv) == 2:
+###
+###		if 'start' == sys.argv[1]:
+###			try:
+###				daemon.start()
+###			except:
+###				pass
+###
+###		elif 'stop' == sys.argv[1]:
+###			print "Stopping ..."
+###			daemon.stop()
+###
+###		elif 'restart' == sys.argv[1]:
+###			print "Restaring ..."
+###			daemon.restart()
+###
+###		elif 'status' == sys.argv[1]:
+###			try:
+###				pf = file(PIDFILE,'r')
+###				pid = int(pf.read().strip())
+###				pf.close()
+###			except IOError:
+###				pid = None
+###			except SystemExit:
+###				pid = None
+###
+###			if pid:
+###				print 'YourDaemon is running as pid %s' % pid
+###			else:
+###				print 'YourDaemon is not running.'
+###
+###		else:
+###			print "Unknown command"
+###			sys.exit(2)
+###			sys.exit(0)
+###	else:
+###		print "usage: %s start|stop|restart|status" % sys.argv[0]
+###		sys.exit(2)
