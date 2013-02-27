f2233be4194ef5b028705cb1d2177bba9f5be1dc
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2013-02-27 13:30:03 +0000

    Modified samslogs.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@289 ab604922-b16d-4620-a341-c5f183439689

--- sublime/samslogs.py
+++ sublime/samslogs.py
@@ -1,48 +1,46 @@
-import sublime, sublime_plugin
 import os, re
+import sublime, sublime_plugin
 
-#Feb 18 11:00:05 icu-f01 Cumain v1.27[247]: ICU System clock adjusted by 0 sec 5971 usec
-class FoldClockCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		regions = self.view.find_all('(\w+ \d{2} \d{2}:\d{2}:\d{2} icu-f01 Cumain v1.27\[\d+\]: ICU System clock adjusted by .*sec\n)+')
-		if regions:
-			self.view.fold(regions)
+PATTERNS_TO_FOLD = [
+# Feb 18 11:00:05 icu-f01 Cumain v1.27[247]: ICU System clock adjusted by 0 sec 5971 usec
+'(\w+\s+\d{1,2} \d{2}:\d{2}:\d{2} icu-f01 Cumain v1.27\[\d+\]: ICU System clock adjusted by .*sec\n)+',
+# Feb 11 11:59:14 icu-f01 rarpd[1305]: ep1: 0:60:97:94:35:53
+'(\w+\s+\d{1,2} \d{2}:\d{2}:\d{2} icu-f01 rarpd\[\d+\]: ep1: 0:\d{2}:\d{2}:\d{2}:\d{2}:\d{2}\n)+',
+# Feb 18 06:42:29 icu-f01 /netbsd: APM ioctl get power status: unknown error code (0x530a)
+'(\w+\s+\d{1,2} \d{2}:\d{2}:\d{2} icu-f01 /netbsd: APM ioctl get power status: unknown error code \(0x530a\)\n)+',
+# Feb 25 09:59:59 icu-f01 newsyslog[29283]: logfile turned over
+'(\w+\s+\d{1,2} \d{2}:\d{2}:\d{2} icu-f01 newsyslog\[\d+\]: logfile turned over\n)+',
+]
 
-#Feb 11 11:59:14 icu-f01 rarpd[1305]: ep1: 0:60:97:94:35:53
-class FoldRarpdCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		regions = self.view.find_all('(\w+ \d{2} \d{2}:\d{2}:\d{2} icu-f01 rarpd\[\d+\]: ep1: 0:\d{2}:\d{2}:\d{2}:\d{2}:\d{2}\n)+')
-		if regions:
-			self.view.fold(regions)
+FILES_TO_FOLD = [
+'.*/var/log/messages',
+'.*/var/log/sams-ii/messages',
+'.*/var/log/sams-ii/watchdoglog',
+]
 
-#Feb 18 06:42:29 icu-f01 /netbsd: APM ioctl get power status: unknown error code (0x530a)
-class FoldApmCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		regions = self.view.find_all('(\w+ \d{2} \d{2}:\d{2}:\d{2} icu-f01 /netbsd: APM ioctl get power status: unknown error code \(0x530a\)\n)+')
-		if regions:
-			self.view.fold(regions)
+class FoldCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        for pattern in PATTERNS_TO_FOLD:
+            #regions = sorted(self.view.find_all(self.pattern), key=lambda x: x.begin(), reverse=True)
+            regions = self.view.find_all(pattern)
+            if regions:
+                for region in regions:
+                    content = self.view.substr(region) + '\n' # in effect, we want to not fold the last...
+                    self.view.replace(edit, region, content)  # ...newline char, so cheat by adding one!
+                regions = self.view.find_all(pattern)
+                self.view.fold(regions)
+
+class InsertNoteCommand(sublime_plugin.TextCommand):
+    def run(self, edit, msg='No note inserted.\n'):
+        self.view.insert(edit, 0, msg)
 
 class SamsLogsFold(sublime_plugin.EventListener):
-	def on_load(self, view):
-		files_handlers = [
-			['.*/var/log/messages',         'rarpd', 'apm'],
-			['.*/var/log/sams-ii/messages', 'clock'],
-		]
-		file_name = view.file_name()
-		if file_name:
-			for expr_handlers in files_handlers:
-				print 'checking', expr_handlers
-				pat = expr_handlers[0]
-				handlers = expr_handlers[1:]
-				print pat
-				print handlers
-				match = re.search(pat, file_name, re.IGNORECASE)
-				if match:
-					print file_name, 'will handle these:',
-					for h in handlers:
-						print h,
-						mapping = 'fold_' + h
-						view.run_command(mapping)
-					print 'done'
-				else:
-					print 'NOT HANDLING', file_name
+    def on_load(self, view):
+        file_name = view.file_name()
+        if file_name:
+            #msg = ['KH NOTE: [']
+            for file_pattern in FILES_TO_FOLD:
+                match = re.search(file_pattern, file_name, re.IGNORECASE)
+                if match:
+                    view.run_command('fold')
+                    break
