415c3f013c784c82a0f2f5e922e83a9ae78ca564
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2013-07-03 11:47:07 +0000

    Added quarantined demo to recipes_fileutils.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@329 ab604922-b16d-4620-a341-c5f183439689

--- recipes/recipes_fileutils.py
+++ recipes/recipes_fileutils.py
@@ -22,6 +22,30 @@ def filter_filenames(dirpath, predicate):
             if predicate(abspath):
                 yield abspath
 
+def put_in_quarantined(fname):
+    import shutil
+    d = os.path.dirname(fname)
+    f = os.path.basename(fname)
+    q = os.path.join(d, 'quarantined')
+    s = os.path.join(d, fname.replace('.header','*') )
+    if not os.path.isdir(q):
+        os.makedirs(q)
+    shutil.move( fname.replace('.header',''), q)
+    shutil.move( fname, q)
+    return
+
+def demo_grep_matches(dirpath, pattern, query):
+    """walk dirpath and show regex matches of filenamePattern that contain query string"""
+    fullfile_pattern = os.path.join(dirpath, pattern)
+    print 'filter_filenames matching pattern "%s"\n================================' % fullfile_pattern
+    for fname in filter_filenames(dirpath, re.compile(fullfile_pattern).match):
+        with open(fname) as f:
+                if query not in f.read():
+                    #print '%s DOES NOT HAVE "%s"' % (fname, query)
+                    put_in_quarantined(fname)
+                #else:
+                #    print '%s HAS "%s"' % (fname, query)
+
 def demo_show_matches(dirpath, pattern):
     """walk dirpath and show regex matches of filenamePattern"""
     fullfile_pattern = os.path.join(dirpath, pattern)
@@ -57,11 +81,23 @@ def demo_show_file_deltas(dirpath, pattern):
     return file_info
 
 if __name__ == "__main__":
+    
+    #######################################################################
+    # Simply walk/show files (recursively under dirpath) that match pattern
     #dirpath = '/home/pims/temp/sams/samslogs014'
     #pattern = '.*\.gz$'
     #demo_show_matches(dirpath, pattern)
-    dirpath = '/tmp/pad/year2013/month06/day27/sams2_accel_121f02'
-    pattern = '.*\.header$'
-    finfo = demo_show_file_deltas(dirpath, pattern)
-    for i in sorted(finfo, key=lambda x: x[-1]):
-        print i[0], i[-1]
+    
+    #######################################################################
+    # Forgot what this does, something to do with files' modified times   
+    #dirpath = '/tmp/pad/year2013/month06/day28/sams2_accel_121f02'
+    #pattern = '.*\.header$'
+    #finfo = demo_show_file_deltas(dirpath, pattern)
+    #for i in sorted(finfo, key=lambda x: x[0]):
+    #    print i[0], i[-1]
+
+    ##################################################################################################################
+    # Do walk/search files (recursively under dirpath) that fname matches pattern & file does NOT contain query string
+    dirpath = '/misc/yoda/pub/pad/year2013/month06'
+    pattern = '.*\.121f04.header$'
+    demo_grep_matches(dirpath, pattern, 'SampleRate>500.0')
