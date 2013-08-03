e561b2bc2d83eddfacc76cd32541cf7086abaefc
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2013-08-03 12:11:51 +0000

    Added recipes_progressbar.py and embellished on recipes_pivot_example.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@335 ab604922-b16d-4620-a341-c5f183439689

--- recipes/recipes_pivot_example.py
+++ recipes/recipes_pivot_example.py
@@ -3,11 +3,12 @@
 import sys
 from pyvttbl import DataFrame
 from dateutil import parser, relativedelta
+import numpy as np
 
 def convert_relativedelta_to_seconds(rdelta):
     return rdelta.days*86400.0 + rdelta.hours*3600.0 + rdelta.minutes*60.0 + rdelta.seconds + rdelta.microseconds/1.0e6
 
-def main(fname):
+def demo1(fname):
     df = DataFrame()
     df.read_tbl(fname)
     df['gmtStart'] = [ parser.parse(i) for i in df['start'] ]
@@ -22,6 +23,14 @@ def main(fname):
     del df['start'], df['stop']
     
     print df
-    
+
+def demo2(fname):
+    df = DataFrame()
+    df.read_tbl(fname)
+    df['approx'] = [ np.around(i,decimals=0) for i in df['pct'] ]
+    pt = df.pivot('approx', ['gmt'], ['sensor'])
+    print pt
+   
 if __name__ == '__main__':
-    main( sys.argv[1] )
+    #demo1( sys.argv[1] )
+    demo2( sys.argv[1] )
