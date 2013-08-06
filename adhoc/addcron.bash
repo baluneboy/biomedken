26c366d4020456ba0e38f0637d8fb87e42cf6852
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2013-08-06 19:28:27 +0000

    Removed blank line and added ampersand in addcron.bash.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@343 ab604922-b16d-4620-a341-c5f183439689

--- adhoc/addcron.bash
+++ adhoc/addcron.bash
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-
-line=" 50 18 06 08 * /home/pims/adhoc/script.py > /tmp/trash.txt # KEEP THIS COMMENT FOR ADHOC CONTROL" 
+line=" 50 18 06 08 * /home/pims/adhoc/script.py &> /tmp/trash.txt # KEEP THIS COMMENT FOR ADHOC CONTROL" 
 ( crontab -l | grep -v "KEEP THIS COMMENT FOR ADHOC CONTROL"; echo "$line" ) | crontab -
