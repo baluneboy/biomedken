027016a177223f8ae12dc95720aac24a03f5439a
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2013-10-11 00:46:53 +0000

    Started work on pdfjam code (property handler class).
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@364 ab604922-b16d-4620-a341-c5f183439689

--- pims/core/files/base.py
+++ pims/core/files/base.py
@@ -29,8 +29,7 @@ class File(object):
     def __repr__(self):
         return "<%s: %s>" % (self.__class__.__name__, self or "None")
 
-    def __len__(self):
-        return self.size
+    def __len__(self): return self.size
 
     def _get_size(self):
         if self.exists():
@@ -39,8 +38,7 @@ class File(object):
             raise AttributeError('Unable to determine the file size for "%s".' % self.name)
         return self._size
 
-    def _set_size(self, size):
-        self._size = size
+    def _set_size(self, size): self._size = size
 
     size = property(_get_size, _set_size)
 
@@ -58,18 +56,10 @@ class RecognizedFile(File):
         super(RecognizedFile, self).__init__(name, show_warnings=show_warnings)
         self.pattern = pattern
         self.recognized = None
-        self._type = None
-        self._why = None
         if not self.is_recognized(): raise UnrecognizedPimsFile('"%s"' % self.name)
 
     def __str__(self):
-        return '%s object for recognized PIMS file "%s" because %s' % (self.__class__.__name__, self.name, self.why() )
-    
-    def why(self):
-        raise NotImplementedError('requires a derived class to override this method')
-    
-    def type(self):
-        raise NotImplementedError('requires a derived class to override this method')
+        return '%s object for recognized PIMS file "%s"' % (self.__class__.__name__, self.name)
 
     def is_recognized(self):
         raise NotImplementedError('requires a derived class to override this method')
@@ -81,20 +71,8 @@ class StupidRecognizedFile(RecognizedFile):
     def __init__(self, name, pattern='.*stupid.*', show_warnings=False):
         super(StupidRecognizedFile, self).__init__(name, pattern, show_warnings=show_warnings)
     
-    def why(self):
-        if not self._why:
-            self._why = 'it contains the string "stupid"'
-        return self._why
-
-    def type(self):
-        if not self._type:
-            self._type = 'a_stupid_but_still_recognized_file'
-        return self._type
-    
     def is_recognized(self):
+        self.recognized = False
         if "stupid" in self.name:
             self.recognized = True
-            self._type = self.type()
-        else:
-            self.recognized = False
         return self.recognized
