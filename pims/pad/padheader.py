fd51fa4ffc7ac97d2059688b7e79e87ee0dbebd6
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2013-10-18 15:10:58 +0000

    Changed PadHeader class in pad/padheader.py to derive from TransformedDict. Modified templates/handbook_template.odt. Now using PadHeaderDict in core/files/handbook.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@386 ab604922-b16d-4620-a341-c5f183439689

--- pims/pad/padheader.py
+++ pims/pad/padheader.py
@@ -4,6 +4,7 @@ PAD Headers.
 """
 
 import os
+import re
 from datetime import timedelta
 from interval import Interval
 from pims.lib.tools import TransformedDict
@@ -14,33 +15,54 @@ from pims.core.files.utils import listdir_filename_pattern
 from pims.utils.iteratortools import pairwise
 from create_header_dict import parse_header # FIXME old [but trusted] code
 
-class PadHeader(object):
+class PadHeaderDict(TransformedDict):
     """
     Given sensor designation and starting datetime, grab unique header (leader).
     
     Examples
     --------
     
-    >>> print PadHeader( '121f03006', datetime(2013, 1, 1))
-    PadHeader for 121f03006 at 2013-01-01 00:00:00
+    >>> print PadHeaderDict( '121f03006', datetime(2013, 1, 1))
+    PadHeaderDict for 121f03006 at 2013-01-01 00:00:00
     
-    >>> print PadHeader( '121f03006', datetime(2012, 12, 31, 0, 5, 0) )._first_header_file
+    >>> print PadHeaderDict( '121f03006', datetime(2012, 12, 31, 0, 5, 0) )._first_header_file
     /misc/yoda/pub/pad/year2012/month12/day31/sams2_accel_121f03006/2012_12_31_00_06_17.985-2012_12_31_01_23_13.189.121f03006.header
     
-    >>> print PadHeader( '121f03006', datetime(2012, 12, 31, 23, 57, 0) )._first_header_file
+    >>> print PadHeaderDict( '121f03006', datetime(2012, 12, 31, 23, 57, 0) )._first_header_file
     /misc/yoda/pub/pad/year2012/month12/day31/sams2_accel_121f03006/2012_12_31_00_06_17.985-2012_12_31_01_23_13.189.121f03006.header
 
-    >>> print PadHeader( '121f03006', datetime(2012, 12, 31, 11, 59, 59, 999000) )._first_header_file
+    >>> print PadHeaderDict( '121f03006', datetime(2012, 12, 31, 11, 59, 59, 999000) )._first_header_file
     /misc/yoda/pub/pad/year2012/month12/day31/sams2_accel_121f03006/2012_12_31_00_06_17.985-2012_12_31_01_23_13.189.121f03006.header
     
     """
     def __init__(self, sensor, desired_start, pad_dir='/misc/yoda/pub/pad'):
-        self.sensor = sensor
-        self.desired_start = desired_start
-        self.pad_dir = pad_dir
-        self.header_file = self._find_the_right_header_file()
-        self.dict = parse_header(self.header_file)
+        super(PadHeaderDict, self).__init__()
+        self['_sensor'] = sensor
+        self['_desired_start'] = desired_start
+        self['_pad_dir'] = pad_dir
+        self['_header_file'] = self._find_the_right_header_file()
+        self._add_hdr_file_fields()
+        self._get_location()
+        self._get_system()
+
+    def __keytransform__(self, key): return key # FIXME for now, just a dict without key transform
+
+    def _add_hdr_file_fields(self):
+        hdr_file_dict = parse_header(self['_header_file'])
+        for k,v in hdr_file_dict.iteritems():
+            self[k] = v
+
+    def _get_system(self):
+        m = re.match( re.compile('(?P<system>sams|mams|mma)(2|es)*_accel'), self['DataType'] )
+        if m:
+            s = m.group('system')
+        else:
+            s = self['DataType'].split('_')[0]
+        self['System'] = s.upper()
         
+    def _get_location(self):
+        self['Location'] = self['SensorCoordinateSystem']['comment']
+    
     def _find_the_right_header_file(self):
         """
         Get list of header files from day before desired_start through day after, then
@@ -55,7 +77,7 @@ class PadHeader(object):
         """
         # Get 3 days worth of header files
         hdr_files = []
-        base_date = self.desired_start.date()
+        base_date = self['_desired_start'].date()
         for day in [ base_date + timedelta(days=x) for x in range(-1, 2) ]:
             hdr_files += self._get_header_files_for_date(day)
 
@@ -69,21 +91,29 @@ class PadHeader(object):
             # Convert time strings to datetime objects
             t1 = timestr_to_datetime(stop1_str)
             t2 = timestr_to_datetime(stop2_str)
-            if self.desired_start > t1 and self.desired_start <= t2:
+            if self['_desired_start'] > t1 and self['_desired_start'] <= t2:
                 #print "*", t1, "to", t2, "<<<", self.desired_start
                 hdr_file = hdr2
                 break
         return hdr_file
 
-    def __str__(self): return '%s for %s at %s' % (self.__class__.__name__, self.sensor, self.desired_start)   
+    def __str__(self):
+        dots = '.' * len(self.__class__.__name__)
+        s = dots
+        s += '\n%s' % self.__class__.__name__
+        for k,v in self.iteritems():
+            if not k.startswith('_'):
+                s += '\n%s: %s' % (k, v)
+        s += '\n%s' % dots
+        return s
 
     def __repr__(self): return self.__str__()
     
     def _get_header_files_for_date(self, d):
         """Get header files for given sensor on d day."""
         # Components of PAD path pattern
-        ymd_subdir = os.path.join( self.pad_dir, datetime_to_pad_ymd_subdir(d) )
-        sys_sensor_pattern = '(?P<system>.*)_accel_%s\Z' % self.sensor
+        ymd_subdir = os.path.join( self['_pad_dir'], datetime_to_pad_ymd_subdir(d) )
+        sys_sensor_pattern = '(?P<system>.*)_accel_%s\Z' % self['_sensor']
         
         # Verify exactly one subdir matches pattern
         matching_dirs = listdir_filename_pattern(ymd_subdir, sys_sensor_pattern)
@@ -91,15 +121,19 @@ class PadHeader(object):
         sensor_dir = matching_dirs[0]
         
         # Get header files
-        header_pattern = '.*\.%s\.header' % self.sensor
+        header_pattern = '.*\.%s\.header' % self['_sensor']
         return listdir_filename_pattern(sensor_dir, header_pattern)        
 
 def demo():
-    ph = PadHeader( '121f03006', datetime(2012, 12, 31, 11, 59, 59, 999000) )
-    print ph.dict
-    #print PadHeader( '121f03006', datetime(2012, 12, 31, 0, 5, 0) ).header_file
-    #print PadHeader( '121f03006', datetime(2012, 12, 31, 23, 57, 0) ).header_file
-    
+    dtm = datetime(2012, 12, 31, 11, 59, 59, 999000)
+    sensors = ['121f03006', 'hirap', '121f02']
+    for sensor in sensors:
+        print '-' * 15
+        ph = PadHeaderDict( sensor, dtm )
+        keys = ['System', 'CutoffFreq', 'SampleRate', 'Location']
+        for key in keys:
+            print key, ph[key]
+   
 if __name__ == "__main__":
     from datetime import datetime
     import doctest
