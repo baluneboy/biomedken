a24354f93bb251421edae26f8604442fd1a875ba
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2013-11-29 14:29:05 +0000

    Refined updating methods in gui/iogrids.py and gui/tally_grid.py. Improved _PADHEADERFILES_PATTERN in patterns/dailyproducts.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@445 ab604922-b16d-4620-a341-c5f183439689

--- pims/patterns/dailyproducts.py
+++ pims/patterns/dailyproducts.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python
 
+# FIXME the file separator character (slash) is for Linux and Mac (not Windows)
+
 __all__ = [
     '_PADHEADERFILES_PATTERN',
     '_BATCHROADMAPS_PATTERN',
     ]
 
 #/misc/yoda/pub/pad/year2013/month11/day01/sams2_accel_121f05/2013_11_01_23_52_29.944-2013_11_02_00_02_29.959.121f05.header
-_PADHEADERFILES_PATTERN = (
-    "(?P<ymdpath>.*)"                                       # perhaps a path at the start, then
+_PADPATH_PATTERN = "(?P<ymdpath>/misc/yoda/pub/pad/year\d{4}/month\d{2}/day\d{2}/)"
+_PADHEADERFILES_PATTERN = _PADPATH_PATTERN + (
+    "(?P<subdir>.*_(?P<sensor>.*))/"                        # subdir
     "(?P<start>\d{4}_\d{2}_\d{2}_\d{2}_\d{2}_\d{2}\.\d{3})" # underscore-delimited start part of fname, then
     "(?P<pm>[\+\-])"                                        # plus/minus
     "(?P<stop>\d{4}_\d{2}_\d{2}_\d{2}_\d{2}_\d{2}\.\d{3})"  # underscore-delimited stop part of fname, then
-    "\.(?P<sensor>.*)"                                      # dot sensor
+    "\.(?P=sensor)"                                         # dot sensor
     "\.header\Z"                                            # pdf extension to finish
     )
 
-
 #/misc/yoda/www/plots/batch/year2013/month09/day29/2013_09_29_00_00_00.000_121f03_spgs_roadmaps500.pdf
-_BATCHROADMAPS_PATTERN = (
-    "(?P<ymdpath>.*)"                                       # perhaps a path at the start, then
+_BATCHPATH_PATTERN = "(?P<ymdpath>/misc/yoda/www/plots/batch/year\d{4}/month\d{2}/day\d{2}/)"
+_BATCHROADMAPS_PATTERN = _BATCHPATH_PATTERN + (
     "(?P<start>\d{4}_\d{2}_\d{2}_\d{2}_\d{2}_\d{2}\.\d{3})" # underscore-delimited dtm part of fname, then
     "_(?P<sensor>.*)_(?P<abbrev>.*)_roadmaps(?P<rate>.*)"   # placeholders for sensor, plot type, rate, then
     "\.pdf\Z"                                               # pdf extension to finish
@@ -33,5 +35,11 @@ if __name__ == "__main__":
     #m = match_pattern_demo('/misc/yoda/www/plots/batch/year2013/month09/day29/2013_09_29_00_00_00.000_121f03_spgs_roadmaps500.pdf', _BATCHROADMAPS_PATTERN)
     #print m.group('sensor'), m.group('dtm')
 
+    #/misc/yoda/pub/pad/year2013/month01/day02/sams2_accel_121f05006/2013_01_02_05_16_08.758+2013_01_02_05_29_47.061.121f05006.header
+    #(?P<ymdpath>.*)(?P<start>\d{4}_\d{2}_\d{2}_\d{2}_\d{2}_\d{2}\.\d{3})(?P<pm>[\+\-])(?P<stop>d{4}_\d{2}_\d{2}_\d{2}_\d{2}_\d{2}\.\d{3})\.(?P<sensor>.*f0[358].*)\.header
+
     m = match_pattern_demo('/misc/yoda/pub/pad/year2013/month11/day01/sams2_accel_121f05/2013_11_01_23_52_29.944-2013_11_02_00_02_29.959.121f05.header', _PADHEADERFILES_PATTERN)
-    print m.group('sensor'), m.group('start')
+    if m:
+        print m.group('sensor'), m.group('start')
+    else:
+        print 'no match'
