1f7dec3cfb0bcfc56ccf5a3c6ac1a43896fb9669
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2013-12-06 09:53:22 +0000

    Added quick_test to realtime/obspy_db_client.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@456 ab604922-b16d-4620-a341-c5f183439689

--- pims/realtime/obspy_db_client.py
+++ pims/realtime/obspy_db_client.py
@@ -2,17 +2,23 @@
 
 from obspy.db.client import Client
 
-#def quick_test():
-#    # 'mysql+mysqldb://<user>:<password>@<host>[:<port>]/<dbname>'
-#    # 'mysql://username:password@serverlocation/mysqldb_databasename?charset=utf8&use_unicode=0'
-#    engine = create_engine('mysql://pims:pims2000@localhost/pims?charset=utf8&use_unicode=0', pool_recycle=3600)
-#    connection = engine.connect()
-#    result = engine.execute("select time from 121f03 order by time desc limit 11")
-#    for row in result:
-#        print "time:", row['time']
-#    result.close()
-#quick_test()
-#raise SystemExit
+def quick_test():
+    from sqlalchemy import create_engine
+    # 'mysql+mysqldb://<user>:<password>@<host>[:<port>]/<dbname>'
+    # 'mysql://username:password@serverlocation/mysqldb_databasename?charset=utf8&use_unicode=0'
+    engine = create_engine('mysql://pims:pims2000@localhost/pims?charset=utf8&use_unicode=0')
+    connection = engine.connect()
+    result = engine.execute("select time from 121f03 order by time desc limit 9")
+    for row in result:
+        print "time:", row['time']
+    r2 = engine.execute('select from_unixtime(time) as gmt from 121f03 order by time desc limit 6')
+    for r in r2:
+        print "GMT:", r['gmt']
+    result.close()
+    r2.close()
+    
+quick_test()
+raise SystemExit
 
 class PimsClient(Client):
     """
