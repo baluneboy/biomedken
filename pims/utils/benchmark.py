f7335704d5024607c6dac81b32bdf725291a4c68
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2013-12-27 20:07:40 +0000

    Put benchmark and set title method into gui/stripchart.py.  Added header_string and warnings filter in pad/packetfeeder.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@496 ab604922-b16d-4620-a341-c5f183439689

--- pims/utils/benchmark.py
+++ pims/utils/benchmark.py
@@ -16,26 +16,40 @@ class Benchmark(object):
     
     def __init__(self, label):
         self.label = label
-        self.seconds = 0.0
+        self.totalsec = 0.0
         self.count = 0
+        # reverse polarity so the data takes over these roles
+        # also do this "smartly" to avoid computational overhead
+        self.min = 1e6
+        self.max = -1
         
     def __str__(self):
-        avg = self.get_avg()
-        fmt = 'benchmark {0:s}: n = {1:d}, avg = {2:.1f}, total = {3:.1f}s'
-        return fmt.format(self.label, self.count, avg, self.seconds)
+        avg = self.get_stats()
+        fmt = 'benchmark (sec) {0:s}: count = {1:d}, min/avg/max = {2:.1f} / {3:.1f} / {4:.1f}, total = {5:.1f}s'
+        return fmt.format(self.label, self.count, self.min, avg, self.max, self.totalsec)
                 
     def start(self):
         self.start_time = time()
         
-    def get_avg(self):
+    def get_stats(self):
         self.count += 1
-        self.seconds += time() - self.start_time
-        return self.seconds / self.count
+        sec_since_start = time() - self.start_time
+        self.totalsec += sec_since_start
+        if sec_since_start < self.min:
+            self.min = sec_since_start
+        if sec_since_start > self.max:
+            self.max = sec_since_start
+        return self.totalsec / self.count
 
 def demo():
     from time import sleep
     bm = Benchmark('demo')
-    for i in range(5):
+    for i in range(2,0,-1):
+        bm.start()
+        sleep(i)
+        print bm
+    print '- - -'
+    for i in range(3,6):
         bm.start()
         sleep(i)
         print bm
