9a0b372b5d95ad4131891cc98113d6426d08d132
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2014-01-06 22:59:06 +0000

    Added pad/tests/test_plotdatasortedlist.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@512 ab604922-b16d-4620-a341-c5f183439689

--- pims/gui/plotutils.py
+++ pims/gui/plotutils.py
@@ -1,40 +1,9 @@
 #!/usr/bin/env python
 
 import numpy as np
-from blist import sortedlist
 
 def smart_ylims(minval, maxval):
     span = maxval - minval
     ymin = np.ceil( minval - 0.1 * span )
     ymax = np.ceil( maxval + 0.1 * span )
     return (ymin, ymax)
-
-class PlotDataSortedList(sortedlist):
-    
-    def __init__(self, *args, **kwargs):
-        kwargs['key'] = lambda tup: tup[0]
-        if kwargs.has_key('maxlen'):
-            self.maxlen = int( kwargs.pop('maxlen') )
-        else:
-            self.maxlen = 999
-        super(PlotDataSortedList, self).__init__(*args, **kwargs)
-
-    def _add(self, *args, **kwargs):
-        super(PlotDataSortedList, self).add(*args, **kwargs)
-            
-    def append(self, txyz):
-        self._add( txyz )
-        while len(self) > self.maxlen:
-            toss = self.pop(0)
-            print "toss", toss
-
-data = PlotDataSortedList(maxlen=4)
-
-for i in range(11,0,-1):
-    data.append( (i, i/10.0) )
-    print data
-    
-data.append( (10.5, 'x') )
-data.append( (10.6, 'y') )
-print data
-    
--- pims/pad/padstream.py
+++ pims/pad/padstream.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-#from obspy import UTCDateTime, Trace
 from obspy import Stream
-from pims.utils.iterabletools import quantify
+from blist import sortedlist
 
+# acceleration data stream with channel/time sorting
 class PadStream(Stream):
+    """acceleration data stream with channel/time sorting"""
     
     def __str__(self, extended=False):
         """Returns short summary string of the current stream."""
@@ -25,16 +26,42 @@ class PadStream(Stream):
         super(PadStream, self).sort(keys=keys, reverse=reverse)
 
     def span(self):
-        """get times"""
+        """Return timespan in seconds."""
         self.sort()
         span = self[-1].stats.endtime - self[0].stats.starttime
         return span
 
-    def get_trace_counts(self):
-        """are we only keeping 28pt packets?"""
-        return [tr.stats.npts for tr in self.traces]
+# container for sorted list of (t,x,y,z) tuples
+class PlotDataSortedList(sortedlist):
+    """container for sorted list of (t,x,y,z) tuples"""
+    
+    def __init__(self, *args, **kwargs):
+        # intentionally clobber key; set to sort by tuple's first element
+        kwargs['key'] = lambda tup: tup[0]
+        if kwargs.has_key('maxlen'):
+            self.maxlen = int( kwargs.pop('maxlen') )
+        else:
+            self.maxlen = 123456
+        super(PlotDataSortedList, self).__init__(*args, **kwargs)
+
+    def __add(self, *args, **kwargs):
+        # note leading double-underscore for private method
+        super(PlotDataSortedList, self).add(*args, **kwargs)
+    
+    def add(self, *args, **kwargs):
+        raise AttributeError('"add" method does not work for %s, try "append" method' % self.__class__.__name__)
+    
+    def append(self, txyz):
+        self.__add( txyz )
+        while len(self) > self.maxlen:
+            toss = self.pop(0)
+
+d = PlotDataSortedList()
+d.append( (1,2) )
+d.add( (1,2) )
 
 def demo_ingest(offset):
+    from pims.utils.iterabletools import quantify
     # Trace 1: 1111
     # Trace 2:          555
     # 1 + 2  : 1111.....555
@@ -46,8 +73,6 @@ def demo_ingest(offset):
     tr2 = Trace(data=np.ones(2, dtype=np.int32) * 2)
     tr2.stats.starttime = tr1.stats.endtime + ( tr1.stats.delta + offset )
     stream = PadStream([tr1, tr2])
-    print stream.get_trace_counts()
-    print "two or three", quantify( stream.get_trace_counts(), lambda i: i == 2 or i == 3 )
     print "span", stream.span()
     for tr in stream: print tr
     stream.verify()
@@ -57,7 +82,6 @@ def demo_ingest(offset):
     print ( tr1.stats.delta + offset )
     print stream[-1][:], "mean=", stream[-1][:].mean(), "std=", stream[-1][:].std()
     print '-' * 100
-    print stream.get_trace_counts()
     
 if __name__ == "__main__":
     for offset in range(-200,200,50):
