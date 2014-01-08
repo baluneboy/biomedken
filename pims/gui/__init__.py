110634282fca4732a6b03a28378b982a5e11448a
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2014-01-08 19:51:20 +0000

    Improved DUMMYDATA in gui/__init__.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@517 ab604922-b16d-4620-a341-c5f183439689

--- pims/gui/__init__.py
+++ pims/gui/__init__.py
@@ -8,7 +8,7 @@ from obspy import UTCDateTime
 from pims.utils.pimsdateutil import unix2dtm
 
 DUMMYDATA = {}
-DUMMYDATA['x'] = np.array([unix2dtm(u) for u in [
+x = np.array([unix2dtm(u) for u in [
     UTCDateTime(2013, 12, 31, 23, 57, 0),
     UTCDateTime(2013, 12, 31, 23, 58, 0),
     UTCDateTime(2013, 12, 31, 23, 59, 0),
@@ -16,4 +16,8 @@ DUMMYDATA['x'] = np.array([unix2dtm(u) for u in [
     UTCDateTime(2014,  1,  1,  0,  1, 0),
     UTCDateTime(2014,  1,  1,  0,  2, 0),
     UTCDateTime(2014,  1,  1,  0,  3, 0)] ])
-DUMMYDATA['y'] = np.array(range(len(DUMMYDATA['x'])))
+y = np.array(range(len(x)), dtype='float')
+y[3] = np.nan
+DUMMYDATA['x'] = x
+DUMMYDATA['y'] = y
+
