13dd3b0d40e079ff8cd9fed0fd6e59b3159d688a
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2014-01-14 22:57:05 +0000

    Homework on pad/processchain.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@530 ab604922-b16d-4620-a341-c5f183439689

--- pims/pad/tests/test_processchain.py
+++ pims/pad/tests/test_processchain.py
@@ -3,6 +3,7 @@
 import unittest
 import os
 import pickle
+import numpy as np
 from copy import deepcopy
 from obspy import UTCDateTime, Trace, read
 from pims.pad.padstream import PadStream
@@ -17,11 +18,31 @@ class PadProcessChainTestCase(unittest.TestCase):
         # path relative to this module (file) is where we save pickled substream (p) file
         pickled_substream = os.path.join( os.path.dirname(__file__), 'substream.p')
         self.saved_substream = pickle.load( open(pickled_substream, 'rb') )
-        self.pad_process_chain = PadProcessChain()
+        # concoct one of our own
+        self.mu, self.sigma = 8.76, 0.54 # mean and standard deviation
+        header = {'network': 'KH', 'station': 'BANG',
+                  'starttime': UTCDateTime(2001, 5, 3, 23, 59, 59, 999000),
+                  'npts': 5001, 'sampling_rate': 500.0,
+                  'channel': 'x'}        
+        tracex = Trace(data=np.random.normal(self.mu, self.sigma, 5001).astype('float64'),
+                       header=deepcopy(header))
+        header['channel'] = 'y'
+        tracey = Trace(data=np.random.normal(self.mu, self.sigma, 5001).astype('float64'),
+                       header=deepcopy(header))
+        header['channel'] = 'z'
+        tracez = Trace(data=np.random.normal(self.mu, self.sigma, 5001).astype('float64'),
+                       header=deepcopy(header))
+        self.valid_substream = PadStream(traces=[tracex, tracey, tracez])
 
     def test_is_valid_substream(self):
         """Valid substream!?"""
         self.assertEqual( True, self.saved_substream.is_valid_substream() )
+        self.assertEqual( True, self.valid_substream.is_valid_substream() )
+        tracex = self.valid_substream[0]
+        tracez = self.valid_substream[2]
+        tracey = self.valid_substream[1]
+        wrong_order_stream = PadStream(traces=[tracex, tracez, tracey])
+        self.assertNotEqual( True, wrong_order_stream )
 
     def test_get_units(self):
         """Test _get_units method."""
@@ -32,6 +53,12 @@ class PadProcessChainTestCase(unittest.TestCase):
         # try to initialize with bad scale_factor
         kwargs = {'scale_factor':999.9}
         self.assertRaises(ValueError, PadProcessChain, **kwargs )
+
+    #def test_scale(self):
+    #    """Test data scaling via scale_factor attribute."""
+    #    ppc = PadProcessChain()
+    #    tr = 
+    #    ppc.scale(tr)
 
 def suite():
     return unittest.makeSuite(PadProcessChainTestCase, 'test')
