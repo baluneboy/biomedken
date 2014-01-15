d7228b5cca4db3fd56f0ec88882428537e8a1b74
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2014-01-15 14:58:11 +0000

    Added function to overwrite file with non-ascii characters removed in files/utils.py and string routine for this in strings/utils.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@531 ab604922-b16d-4620-a341-c5f183439689

--- pims/pad/tests/test_processchain.py
+++ pims/pad/tests/test_processchain.py
@@ -8,6 +8,25 @@ from copy import deepcopy
 from obspy import UTCDateTime, Trace, read
 from pims.pad.padstream import PadStream
 from pims.pad.processchain import PadProcessChain
+from pims.signal.ramp import ramp
+
+def build_ramps(npts):
+    slopes = [11.1, 0, -9.9] # x, y, and z
+    intercepts = [-5.0, -3.0, 1.0]
+    t = np.linspace(0, 20*np.pi, npts)
+    x = ramp(t, slope=slopes[0], yint=intercepts[0], noise=True, noise_amplitude=5)
+    y = ramp(t, slope=slopes[1], yint=intercepts[1], noise=True, noise_amplitude=5)
+    z = ramp(t, slope=slopes[2], yint=intercepts[2], noise=True, noise_amplitude=5)
+    header = {'network': 'KH', 'station': 'RAMP',
+              'starttime': UTCDateTime(2011, 12, 10, 6, 30, 00),
+              'npts': 5001, 'sampling_rate': 500.0,
+              'channel': 'x'}        
+    tracex = Trace(data=x, header=deepcopy(header))
+    header['channel'] = 'y'
+    tracey = Trace(data=y, header=deepcopy(header))
+    header['channel'] = 'z'
+    tracez = Trace(data=z, header=deepcopy(header))    
+    return tracex, tracey, tracez
 
 class PadProcessChainTestCase(unittest.TestCase):
     """
@@ -18,7 +37,7 @@ class PadProcessChainTestCase(unittest.TestCase):
         # path relative to this module (file) is where we save pickled substream (p) file
         pickled_substream = os.path.join( os.path.dirname(__file__), 'substream.p')
         self.saved_substream = pickle.load( open(pickled_substream, 'rb') )
-        # concoct one of our own
+        # create normally-distributed random stream w/ mu, sigma known
         self.mu, self.sigma = 8.76, 0.54 # mean and standard deviation
         header = {'network': 'KH', 'station': 'BANG',
                   'starttime': UTCDateTime(2001, 5, 3, 23, 59, 59, 999000),
@@ -33,7 +52,10 @@ class PadProcessChainTestCase(unittest.TestCase):
         tracez = Trace(data=np.random.normal(self.mu, self.sigma, 5001).astype('float64'),
                        header=deepcopy(header))
         self.valid_substream = PadStream(traces=[tracex, tracey, tracez])
-
+        # create linear ramp random stream, per-axis (x,y,z)
+        tracex, tracey, tracez = build_ramps(5001)
+        self.ramps_substream = PadStream(traces=[tracex, tracey, tracez])
+        
     def test_is_valid_substream(self):
         """Valid substream!?"""
         self.assertEqual( True, self.saved_substream.is_valid_substream() )
@@ -54,14 +76,52 @@ class PadProcessChainTestCase(unittest.TestCase):
         kwargs = {'scale_factor':999.9}
         self.assertRaises(ValueError, PadProcessChain, **kwargs )
 
-    #def test_scale(self):
-    #    """Test data scaling via scale_factor attribute."""
-    #    ppc = PadProcessChain()
-    #    tr = 
-    #    ppc.scale(tr)
+    def test_scale(self):
+        """Test data scaling via scale_factor attribute."""
+        ppc = PadProcessChain()
+        tr = Trace(data=np.ones(9)*17)
+        self.assertEqual( 17.0, np.max([tr]) )
+        ppc.scale(tr)
+        self.assertEqual( 17000000.0, np.max([tr]) )
+        ppc = PadProcessChain(scale_factor=1e3)
+        tr = Trace(data=np.ones(9)*17)
+        self.assertEqual( 17.0, np.max([tr]) )
+        ppc.scale(tr)
+        self.assertEqual( 17000.0, np.max([tr]) )
+
+    def test_detrend(self):
+        """Test data detrend via detrend method on substream."""
+        # simple demean (default)
+        ppc = PadProcessChain()
+        ppc.detrend(self.saved_substream)
+        self.assertAlmostEqual( 0.0, np.mean([self.saved_substream[0]]) )
+        self.assertAlmostEqual( 0.0, np.mean([self.saved_substream[1]]) )
+        self.assertAlmostEqual( 0.0, np.mean([self.saved_substream[2]]) )
+        # now remove linear trend
+        ppc = PadProcessChain(detrend_type='linear')
+        ppc.detrend(self.ramps_substream)
+        self.assertAlmostEqual( 0.0, np.mean([self.ramps_substream[0]]) )
+        self.assertAlmostEqual( 0.0, np.mean([self.ramps_substream[1]]) )
+        self.assertAlmostEqual( 0.0, np.mean([self.ramps_substream[2]]) )        
+
+    def test_filter(self):
+        """Test data filtering via filter method on substream."""
+        # default 5 Hz LPF
+        ppc = PadProcessChain()
+        ppc.filter(self.saved_substream)
+        self.assertAlmostEqual( 0.0, np.mean([self.saved_substream[0]]) )
+        self.assertAlmostEqual( 0.0, np.mean([self.saved_substream[1]]) )
+        self.assertAlmostEqual( 0.0, np.mean([self.saved_substream[2]]) )
 
 def suite():
     return unittest.makeSuite(PadProcessChainTestCase, 'test')
+
+def view_waveforms():
+    tracex, tracey, tracez = build_ramps(5001)
+    ramps_substream = PadStream(traces=[tracex, tracey, tracez])
+    ramps_substream.plot()
+    
+#view_waveforms(); raise SystemExit
 
 if __name__ == '__main__':
     unittest.main(defaultTest='suite')
