f59ae002d7a92b687085c52eafa6552cf11c1637
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2014-01-23 23:28:08 +0000

    Better pivot and totals for SAMS and MAMS in pad/amp_kpi.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk/pims/pad@542 ab604922-b16d-4620-a341-c5f183439689

--- amp_kpi.py
+++ amp_kpi.py
@@ -5,67 +5,32 @@
 
 import re
 import csv
-from pandas import read_csv, pivot_table
+from pandas import read_csv, pivot_table, concat
 import numpy as np
 from legacy_pad_times_tally import *
-
-def crit(label):
-    _SAMS_HOURS_PATTERN = 'sams.*_hours'
-    _MAMS_HOURS_PATTERN = 'mams_ossraw.*_hours|mams_hirap.*_hours'
-    regex = re.compile(_SAMS_HOURS_PATTERN)
-    print label
-    return regex.match(label)
     
 # read CSV into dataframe (for pivot tables)
 def csv2dataframe(csvfile):
     """read CSV into dataframe (for pivot tables)"""
-    ###with open(csvfile, 'rb') as f:
-    ###    labels = f.next().strip().split(',')
-    ###df = DataFrame()
-    ###df.read_tbl(csvfile)
-    
-    ##z = zip(df['sams_121f02_hours'], df['sams_121f03_hours'])
-    ##a  = df['sams_121f02_hours']
-    ##a += df['sams_121f03_hours']
-    ##print z[100:109]
-    ##print a[100:109]
-    ##raise SystemExit
-    ##df['twothree_hours'] = [ a + b for a,b in zip(df['sams_121f02_hours'], df['sams_121f03_hours']) ]
-    ##labels.append('twothree_hours')
-    
-    #args = []
-    #for s in [2,3,4,5,6,8]:
-    #    args.append( df['sams_121f0' + str(s) + '_hours'] )
-    ##args = [ df['sams_121f02_hours'], df['sams_121f03_hours'] ]
-    #sams_sum_hours = []
-    #for t in izip(*args):
-    #    sams_sum_hours.append(np.sum(t))
-    #df['sams_sum_hours'] = sams_sum_hours
-    #labels.append('sams_sum_hours')
-
     with open(csvfile, 'rb') as f:
         labels = f.next().strip().split(',')
     df = read_csv(csvfile)
     
-    print df
-    df.filter(regex='sams.*_hours')
-    print df[:,['sams_121f02_hours', 'sams_es06_hours']]
-
-    raise SystemExit
-
+    # monthly system hours
+    ndf = df.filter(regex='Year|Month|Day|sams.*_hours')
+    cols = [i for i in ndf.columns if i not in ['Year', 'Month', 'Day']]
+    t = pivot_table(ndf, rows=['Year','Month'], values=cols, aggfunc=np.sum)
+    sams_series = t.transpose().sum()
     
+    ndf = df.filter(regex='Year|Month|Day|mams.*_hours')
+    cols = [i for i in ndf.columns if i not in ['Year', 'Month', 'Day']]
+    t = pivot_table(ndf, rows=['Year','Month'], values=cols, aggfunc=np.sum)
+    mams_series = t.transpose().sum()
 
-    sams_columns = [m.group(0) for m in [regex.match(label) for label in labels] if m]
+    pass    
     
-    df_sams_hours = df[sams_columns]
-    #print df_sams_hours
-    print df
-    #col_sums = df_sams_hours.sum()
-
-    #for idx in [0, 199, 255, 777]:
-    #    print idx, np.round( df23[idx] - df['sams_121f02_hours'][idx] - df['sams_121f03_hours'][idx], decimals=6 )
-
-    #return df, labels
+    #total = concat([sams_series, mams_series], axis=0)
+    #print total
 
 def main(csvfile):
     csv2dataframe(csvfile)
