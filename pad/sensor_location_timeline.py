c321da8a87b3c293f00a7dc60a5802440f24ea0f
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2014-01-30 18:05:52 +0000

    Modified pad/sensor_location_timeline.py for timestamping and to give both HTML and CSV outputs.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk/pims@549 ab604922-b16d-4620-a341-c5f183439689

--- pad/sensor_location_timeline.py
+++ pad/sensor_location_timeline.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+import sys
 from pims.database.pimsquery import CoordQueryAsDataFrame
+import datetime
 
 _PREAMBLE = """<html>
   <head>
@@ -18,7 +20,7 @@ _PREAMBLE = """<html>
       // instantiates the timeline chart, passes in the data
       // and draws it.
       function drawChart() {
-        var container = document.getElementById('example');
+        var container = document.getElementById('sams_sensor_timeline');
         var chart = new google.visualization.Timeline(container);
         var dataTable = new google.visualization.DataTable();
         dataTable.addColumn({ type: 'string', id: 'Sensor' });
@@ -41,22 +43,38 @@ _EPILOGUE = """        ]);
   </head>
 
   <body>
+  <big><span style="font-weight: bold;">Updated %s</span></big><br>
     <!--Div that will hold the timeline chart-->
-    <div id="example" style="height: 900px;"></div>
+    <div id="sams_sensor_timeline" style="height: 900px;"></div>
   </body>
-</html>"""
+</html>""" % datetime.datetime.now().strftime('GMT %Y-%m-%d/%H:%M:%S')
 
-def main():
-    c = CoordQueryAsDataFrame(host='localhost')
+def main(html_file):
+
+    csv_file = html_file.replace('.html', '.csv')
+    
+    # grab sensor location info
+    c = CoordQueryAsDataFrame(host='kyle') # or localhost for testing on jimmy
     c.filter_dataframe_sensors('^121f0|^es0|hirap')
     c.filter_pre2001()
+
+    # write CSV to csv_file
+    c.dataframe.to_csv(csv_file, index=False)
+
+    # make HTML sensor_rows succinct (no rpy or xyz)
     c.consolidate_rpy_xyz()
-    #print c.dataframe
     sensor_rows = c.get_rows()
-    print _PREAMBLE
-    print sensor_rows
-    print _EPILOGUE
+    
+    # write HTML to html_file
+    with open(html_file, "w") as text_file:
+        text_file.write(_PREAMBLE)
+        text_file.write(sensor_rows)
+        text_file.write(_EPILOGUE)
+    
+    #print _PREAMBLE
+    #print sensor_rows
+    #print _EPILOGUE
     
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1])
     
