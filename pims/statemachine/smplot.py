f2a7ee6de41693002f1e3fdd6f0fd0d51257f404
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2014-02-26 19:18:59 +0000

    Added sandbox/wxmplot_example1.py, sandbox/simple_mapreduce.py, and sandbox/wxmplot_example2.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@567 ab604922-b16d-4620-a341-c5f183439689

--- pims/statemachine/smplot.py
+++ pims/statemachine/smplot.py
@@ -18,16 +18,19 @@ class PlotParametersStateMachine(StateMachine):
     def __init__(self):
         self.monitor() # start with monitor
 
+    def squawk(self, msg):
+        print '%s: %s' % (self.__class__.__name__, msg)
+
     def on_enter_monitor(self, from_state=None, to_state=None):
         if from_state == None:
             # initial entry point
-            print 'initial entry point, start monitoring'
+            self.squawk( 'initial entry point, start monitoring' )
             return True
         elif from_state == 'problem':
-            print 'reset after problem, restart monitoring'
+            self.squawk( 'reset after problem, restart monitoring' )
             return True
         elif from_state == 'deployed':
-            print 'done with previous deployment, start monitoring'
+            self.squawk( 'done with previous deployment, start monitoring' )
             return True
         else:
             return False
@@ -35,7 +38,7 @@ class PlotParametersStateMachine(StateMachine):
     def on_enter_found(self, from_state=None, to_state=None):
         if from_state == 'monitor':
             # found a file to be processed
-            print 'a file to be processed was found'
+            self.squawk( 'a file to be processed was found' )
             return True
         else:
             return False
@@ -43,7 +46,7 @@ class PlotParametersStateMachine(StateMachine):
     def on_enter_pending(self, from_state=None, to_state=None):
         if from_state == 'found':
             # now do processing
-            print 'file being processed, deployment still pending'
+            self.squawk( 'file being processed, deployment still pending' )
             return True
         else:
             return False
@@ -51,7 +54,7 @@ class PlotParametersStateMachine(StateMachine):
     def on_enter_problem(self, from_state=None, to_state=None, msg=None):
         if from_state == 'pending':
             if msg:
-                print '%s problem: %s' % (self.__class__.__name__, msg)
+                self.squawk( 'problem: %s' %  msg )
             return True
         else:
             return False
@@ -59,7 +62,7 @@ class PlotParametersStateMachine(StateMachine):
     def on_enter_deployed(self, from_state=None, to_state=None):
         if from_state == 'pending':
             # we done did it
-            print 'new file was deployed'
+            self.squawk( 'new file was deployed' )
             return True
         else:
             return False
