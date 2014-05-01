f26545c09f0425f3b7e06dbce456957ba2ff3292
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2014-05-01 13:21:07 +0000

    Started work on blinkstick state machine.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@615 ab604922-b16d-4620-a341-c5f183439689

--- pims/statemachine/smplot.py
+++ pims/statemachine/smplot.py
@@ -2,6 +2,72 @@
 
 from smachine import StateMachine
 
+# a state machine for blinkstick monitoring real-time
+class BlinkstickStateMachine(StateMachine):
+    """a state machine for blinkstick monitoring real-time"""
+    state = None
+    states = ['monitor', 'found', 'pending', 'deployed', 'problem']
+    transitions = {
+        None:       ['monitor'],
+        'monitor':  ['found'],
+        'found':    ['pending'],
+        'pending':  ['deployed', 'problem'],
+        'deployed': ['monitor'],
+        'problem':  ['monitor'],
+    }
+
+    def __init__(self):
+        self.monitor() # start with monitor
+
+    def squawk(self, msg):
+        print '%s: %s' % (self.__class__.__name__, msg)
+
+    def on_enter_monitor(self, from_state=None, to_state=None):
+        if from_state == None:
+            # initial entry point
+            self.squawk( 'initial entry point, start monitoring' )
+            return True
+        elif from_state == 'problem':
+            self.squawk( 'reset after problem, restart monitoring' )
+            return True
+        elif from_state == 'deployed':
+            self.squawk( 'done with previous deployment, start monitoring' )
+            return True
+        else:
+            return False
+
+    def on_enter_found(self, from_state=None, to_state=None):
+        if from_state == 'monitor':
+            # found a file to be processed
+            self.squawk( 'a file to be processed was found' )
+            return True
+        else:
+            return False
+
+    def on_enter_pending(self, from_state=None, to_state=None):
+        if from_state == 'found':
+            # now do processing
+            self.squawk( 'file being processed, deployment still pending' )
+            return True
+        else:
+            return False
+
+    def on_enter_problem(self, from_state=None, to_state=None, msg=None):
+        if from_state == 'pending':
+            if msg:
+                self.squawk( 'problem: %s' %  msg )
+            return True
+        else:
+            return False
+
+    def on_enter_deployed(self, from_state=None, to_state=None):
+        if from_state == 'pending':
+            # we done did it
+            self.squawk( 'new file was deployed' )
+            return True
+        else:
+            return False
+
 # a state machine for plot parameters
 class PlotParametersStateMachine(StateMachine):
     state = None
@@ -67,7 +133,7 @@ class PlotParametersStateMachine(StateMachine):
         else:
             return False
 
-if __name__ == "__main__":
+def demo_plotparams():
     
     # create machine to maintain state info
     machine = PlotParametersStateMachine()
@@ -84,3 +150,25 @@ if __name__ == "__main__":
     for s in sequence:
         machine.push(s)
         machine.next()
+        
+def demo_bstick():
+    
+    # create machine to maintain state info
+    machine = BlinkstickStateMachine()
+    
+    # a bad sequence
+    sequence = ['found', 'pending']
+    for s in sequence:
+        machine.push(s)
+        machine.next()
+    machine.problem('a big issue')
+
+    # a good sequence
+    sequence = ['monitor', 'found', 'pending', 'deployed', 'monitor']
+    for s in sequence:
+        machine.push(s)
+        machine.next()
+        
+if __name__ == "__main__":
+    demo_bstick()
+
