b53cb37536d5216789f3467ba1fc8a8506564217
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2014-05-01 19:27:30 +0000

    Added handbook.komodoproject, blinkstick.komodoproject, sandbox/demo_openpyxl.py, and sandbox/demo_pandas_to_excel.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk/pims@617 ab604922-b16d-4620-a341-c5f183439689

--- blinkstick/demo_blinkstick.py
+++ blinkstick/demo_blinkstick.py
@@ -100,39 +100,13 @@ def demo_set_get_block_info(info_block1, info_block2):
     bstick = blinkstick.find_first()
     
     # set and get device info-block1 here
-    #bstick.set_info_block1(info_block1)
+    bstick.set_info_block1(info_block1)
     print bstick.get_info_block1()
     
     # set and get device info-block2 here
-    #bstick.set_info_block2(info_block2)
+    bstick.set_info_block2(info_block2)
     print bstick.get_info_block2()
     
-demo_set_get_block_info("abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"); raise SystemExit    
-
-def demo_kh():
-
-    # Do processing needed to update blinkstick [output how/where?]
-    #code goes here with status to [syslog?]
-    
-    # Get "state" of blinkstick from one of its two [registers?]
-    #code goes here with "old state" to [syslog?]
-    
-    # Turn blinkstick off
-    bstick.turn_off()
-    
-    # Update blinkstick with info from processing done "not long ago"
-    for x in range(5):
-        bstick.set_random_color()
-        print bstick.get_serial() + " " + bstick.get_color(color_format="hex")
-        time.sleep(0.5)
-    # with "new state" to [syslog?]
-        
-    # Restore "state" of blinkstick    
-    bstick.turn_off()
-    
-    # Log status
-    print bstick.get_serial() + " turned off"
-
 if __name__ == "__main__":
     #bstick.set_random_color(); time.sleep(2); bstick.turn_off(); raise SystemExit
-    demo_kh()
+    demo_set_get_block_info("abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
--- pad/process_sto_file.py
+++ pad/process_sto_file.py
@@ -8,7 +8,34 @@ from pims.pad.amp_kpi import convert_sto2csv
 #stofile = '/misc/yoda/www/plots/batch/padtimes/2014_077-092_cir_fir_pwr_sams2min.sto'
 #stofile = '/misc/yoda/www/plots/user/sams/playback/er34_msg_cir_fir.sto'
 #stofile = '/misc/yoda/www/plots/user/sams/playback/er34_msg_cir_firX.sto'
-stofile = '/misc/yoda/www/plots/user/sams/playback/er34_msg_cir_fir_JanFeb.sto'
+#stofile = '/misc/yoda/www/plots/user/sams/playback/er34_msg_cir_fir_JanFeb.sto'
+#stofile = '/misc/yoda/www/plots/user/sams/kpi/2014_04_sams_monthly_kpi.sto'
 
-if __name__ == "__main__":
+# process sto file to get amp kpi spreadsheet
+def process_amp_kpi():
+    """process sto file to get amp kpi spreadsheet"""
+    
+    # use "last" month to figure out what name of stofile should be instead of input argument
+    #
+    
+    # process stofile
     convert_sto2csv( sys.argv[1] )
+    
+    # EITHER do per-file dataframe with column appending (and necessary row trim/pad)
+    # OR do the next block of code (BUT do not do both)
+
+    ### # for each of grouped.csv files
+    ###     # pad rows with zeros for missing days so we have one for each day of "this" month
+    ###     #
+    ###     
+    ###     # trim first day of "next" month
+    ###     #
+    ### 
+    ### # subprocess LIKE: "paste -d, *grouped.csv > 2014_04_grouped_pasted.csv"
+    ### #
+    
+    # subprocess to zip all EXCEPT for final product spreadsheet
+    #
+    
+if __name__ == "__main__":
+    process_amp_kpi()   
