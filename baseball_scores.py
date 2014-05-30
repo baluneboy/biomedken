97546aa8ec1dc10e3fb083d1f1e19bb557596086
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2014-05-30 12:16:47 +0000

    Minor changes in sandbox/baseball_scores.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk/pims/sandbox@627 ab604922-b16d-4620-a341-c5f183439689

--- baseball_scores.py
+++ baseball_scores.py
@@ -12,6 +12,23 @@ import elementtree.ElementTree as ET
 #url = 'http://scores.nbcsports.msnbc.com/ticker/data/gamesMSNBC.js.asp?jsonp=true&sport=MLB&period=20140525'
 url = 'http://scores.nbcsports.msnbc.com/ticker/data/gamesMSNBC.js.asp?jsonp=true&sport=%s&period=%d'
 
+class MyColors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+def demo_colorize():
+    print MyColors.HEADER + "HEADER: No active frommets remain. Continue?" + MyColors.ENDC    
+    print MyColors.OKBLUE + "OKBLUE: No active frommets remain. Continue?" + MyColors.ENDC    
+    print MyColors.OKGREEN + "OKGREEN: No active frommets remain. Continue?" + MyColors.ENDC    
+    print MyColors.WARNING + "WARNING: No active frommets remain. Continue?" + MyColors.ENDC    
+    print MyColors.FAIL + "FAIL: No active frommets remain. Continue?" + MyColors.ENDC    
+
+#demo_colorize(); raise SystemExit
+
 def yesterday(league):
 
     # one day ago
@@ -58,40 +75,23 @@ def yesterday(league):
 
 def get_example_games():
     return [
-        {'status': 'Final', 'league': 'MLB', 'start': 1401124200, 'home': 'Braves', 'away': 'Red Sox', 'clock': 'Final', 'away-score': '8', 'home-score': '6', 'clock-section': ''},
-        {'status': 'Final', 'league': 'MLB', 'start': 1401124200, 'home': 'Mets', 'away': 'Pirates', 'clock': 'Final', 'away-score': '5', 'home-score': '3', 'clock-section': ''},
-        {'status': 'Final', 'league': 'MLB', 'start': 1401124200, 'home': 'Mets', 'away': 'Pirates', 'clock': 'Final', 'away-score': '5', 'home-score': '3', 'clock-section': ''},
-        {'status': 'Final', 'league': 'MLB', 'start': 1401125700, 'home': 'Nationals', 'away': 'Marlins', 'clock': 'Final', 'away-score': '3', 'home-score': '2', 'clock-section': ''},
-        {'status': 'Final', 'league': 'MLB', 'start': 1401127800, 'home': 'White Sox', 'away': 'Indians', 'clock': 'Final', 'away-score': '2', 'home-score': '6', 'clock-section': ''},
-        {'status': 'Final', 'league': 'MLB', 'start': 1401127800, 'home': 'Brewers', 'away': 'Orioles', 'clock': 'Final', 'away-score': '7', 'home-score': '6', 'clock-section': '10'},
-        {'status': 'Final', 'league': 'MLB', 'start': 1401127800, 'home': 'Twins', 'away': 'Rangers', 'clock': 'Final', 'away-score':  '7', 'home-score': '2', 'clock-section': ''},
-        {'status': 'Final', 'league': 'MLB', 'start': 1401134700, 'home': 'Athletics', 'away': 'Tigers', 'clock': 'Final', 'away-score': '0', 'home-score': '10', 'clock-section': ''},
-        {'status': 'Final', 'league': 'MLB', 'start': 1401134700, 'home': 'Giants', 'away': 'Cubs', 'clock': 'Final', 'away-score': '8', 'home-score': '4', 'clock-section': ''},
-        {'status': 'Final', 'league': 'MLB', 'start': 1401135000, 'home': 'Mariners', 'away': 'Angels', 'clock': 'Final', 'away-score': '1', 'home-score': '5', 'clock-section': ''},
-        {'status': 'Final', 'league': 'MLB', 'start': 1401135300, 'home': 'Cardinals', 'away': 'Yankees', 'clock': 'Final', 'away-score': '6', 'home-score': '4', 'clock-section': '12'},
-        {'status': 'Final', 'league': 'MLB', 'start': 1401138300, 'home': 'Phillies', 'away': 'Rockies', 'clock': 'Final', 'away-score': '0', 'home-score': '9', 'clock-section': ''},
-        {'status': 'Final', 'league': 'MLB', 'start': 1401145620, 'home': 'Blue Jays', 'away': 'Rays', 'clock': 'Final', 'away-score': '5', 'home-score': '10', 'clock-section': ''},
-        {'status': 'Final', 'league': 'MLB', 'start': 1401149400, 'home': 'Royals', 'away': 'Astros', 'clock': 'Final', 'away-score': '9', 'home-score': '2', 'clock-section': ''},
-        {'status': 'Final', 'league': 'MLB', 'start': 1401149400, 'home': 'Dodgers', 'away': 'Reds', 'clock': 'Final', 'away-score': '3', 'home-score': '4', 'clock-section': ''},
-        {'status': 'Final', 'league': 'MLB', 'start': 1401149400, 'home': 'Diamondbacks', 'away': 'Padres', 'clock': 'Final', 'away-score': '5', 'home-score': '7', 'clock-section': ''},
+        {'status': 'Final', 'league': 'MLB', 'start': datetime.datetime.fromtimestamp(1401124200), 'home': 'Braves', 'away': 'Red Sox', 'clock': 'Final', 'away-score': '8', 'home-score': '6', 'clock-section': ''},
+        {'status': 'Final', 'league': 'MLB', 'start': datetime.datetime.fromtimestamp(1401124200), 'home': 'Mets', 'away': 'Pirates', 'clock': 'Final', 'away-score': '5', 'home-score': '3', 'clock-section': ''},
+        {'status': 'Final', 'league': 'MLB', 'start': datetime.datetime.fromtimestamp(1401124200), 'home': 'Mets', 'away': 'Pirates', 'clock': 'Final', 'away-score': '5', 'home-score': '3', 'clock-section': ''},
+        {'status': 'Final', 'league': 'MLB', 'start': datetime.datetime.fromtimestamp(1401125700), 'home': 'Nationals', 'away': 'Marlins', 'clock': 'Final', 'away-score': '3', 'home-score': '2', 'clock-section': ''},
+        {'status': 'Final', 'league': 'MLB', 'start': datetime.datetime.fromtimestamp(1401127800), 'home': 'White Sox', 'away': 'Indians', 'clock': 'Final', 'away-score': '2', 'home-score': '6', 'clock-section': ''},
+        {'status': 'Final', 'league': 'MLB', 'start': datetime.datetime.fromtimestamp(1401127800), 'home': 'Brewers', 'away': 'Orioles', 'clock': 'Final', 'away-score': '7', 'home-score': '6', 'clock-section': '10'},
+        {'status': 'Final', 'league': 'MLB', 'start': datetime.datetime.fromtimestamp(1401127800), 'home': 'Twins', 'away': 'Rangers', 'clock': 'Final', 'away-score':  '7', 'home-score': '2', 'clock-section': ''},
+        {'status': 'Final', 'league': 'MLB', 'start': datetime.datetime.fromtimestamp(1401134700), 'home': 'Athletics', 'away': 'Tigers', 'clock': 'Final', 'away-score': '0', 'home-score': '10', 'clock-section': ''},
+        {'status': 'Final', 'league': 'MLB', 'start': datetime.datetime.fromtimestamp(1401134700), 'home': 'Giants', 'away': 'Cubs', 'clock': 'Final', 'away-score': '8', 'home-score': '4', 'clock-section': ''},
+        {'status': 'Final', 'league': 'MLB', 'start': datetime.datetime.fromtimestamp(1401135000), 'home': 'Mariners', 'away': 'Angels', 'clock': 'Final', 'away-score': '1', 'home-score': '5', 'clock-section': ''},
+        {'status': 'Final', 'league': 'MLB', 'start': datetime.datetime.fromtimestamp(1401135300), 'home': 'Cardinals', 'away': 'Yankees', 'clock': 'Final', 'away-score': '6', 'home-score': '4', 'clock-section': '12'},
+        {'status': 'Final', 'league': 'MLB', 'start': datetime.datetime.fromtimestamp(1401138300), 'home': 'Phillies', 'away': 'Rockies', 'clock': 'Final', 'away-score': '0', 'home-score': '9', 'clock-section': ''},
+        {'status': 'Final', 'league': 'MLB', 'start': datetime.datetime.fromtimestamp(1401145620), 'home': 'Blue Jays', 'away': 'Rays', 'clock': 'Final', 'away-score': '5', 'home-score': '10', 'clock-section': ''},
+        {'status': 'Final', 'league': 'MLB', 'start': datetime.datetime.fromtimestamp(1401149400), 'home': 'Royals', 'away': 'Astros', 'clock': 'Final', 'away-score': '9', 'home-score': '2', 'clock-section': ''},
+        {'status': 'Final', 'league': 'MLB', 'start': datetime.datetime.fromtimestamp(1401149400), 'home': 'Dodgers', 'away': 'Reds', 'clock': 'Final', 'away-score': '3', 'home-score': '4', 'clock-section': ''},
+        {'status': 'Final', 'league': 'MLB', 'start': datetime.datetime.fromtimestamp(1401149400), 'home': 'Diamondbacks', 'away': 'Padres', 'clock': 'Final', 'away-score': '5', 'home-score': '7', 'clock-section': ''},
         ]    
-
-class MyColors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-
-def demo_colorize():
-    print MyColors.HEADER + "HEADER: No active frommets remain. Continue?" + MyColors.ENDC    
-    print MyColors.OKBLUE + "OKBLUE: No active frommets remain. Continue?" + MyColors.ENDC    
-    print MyColors.OKGREEN + "OKGREEN: No active frommets remain. Continue?" + MyColors.ENDC    
-    print MyColors.WARNING + "WARNING: No active frommets remain. Continue?" + MyColors.ENDC    
-    print MyColors.FAIL + "FAIL: No active frommets remain. Continue?" + MyColors.ENDC    
-
-#demo_colorize(); raise SystemExit
 
 if __name__ == "__main__":
     
@@ -102,6 +102,8 @@ if __name__ == "__main__":
     #
     #raise SystemExit
 
+
+    #games = yesterday(['MLB'])
     games = get_example_games()
     for g in games:
         if int(g['away-score']) < int(g['home-score']):
@@ -112,3 +114,4 @@ if __name__ == "__main__":
             acolor = MyColors.OKGREEN
         print '\n%s%s %s%s' % (acolor, g['away'], g['away-score'], MyColors.ENDC) # FIXME LATER becomes %d for int scores
         print '%s%s %s%s' % (hcolor, g['home'], g['home-score'], MyColors.ENDC) # FIXME LATER becomes %d for int scores
+        print 'Started at %s' % g['start']
