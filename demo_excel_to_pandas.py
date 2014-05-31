77a4492e849be83c63940e4bc1d3d9c09fcffb10
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2014-05-31 15:16:19 +0000

    Added excel/ledger.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk/pims/sandbox@630 ab604922-b16d-4620-a341-c5f183439689

--- demo_excel_to_pandas.py
+++ demo_excel_to_pandas.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 
+import datetime
 import pandas as pd
 
 file_name = '/home/pims/Documents/xactions.xlsx'
+file_name = '/home/pims/Documents/example.xlsx'
 xl_file = pd.ExcelFile(file_name)
 
 dfs = {sheet_name: xl_file.parse(sheet_name) for sheet_name in xl_file.sheet_names}
 
-print dfs['xactions']
-print dfs['zin']
+dfx = dfs['xactions']
+dfz = dfs['zin']
+dfv = dfs['vars']
+
+new_row = pd.DataFrame([dict(PayDate=datetime.datetime.now().date(), Hours=80.0, Type='Regular', Amount=123.45), ])
+dfz = dfz.append(new_row, ignore_index=True)
+
+print dfz[-5:]
+print dfv
