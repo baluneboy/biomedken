c4bf2bdf9decbdbdfe443e7a985da7a92fb54d88
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2014-05-31 09:56:43 +0000

    Added readme.txt along /home/pims/dev/programs/python/office.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@628 ab604922-b16d-4620-a341-c5f183439689

--- office/MyCalcSample.py
+++ office/MyCalcSample.py
@@ -5,7 +5,7 @@ def save_document(filename):
     smgr = ctx.ServiceManager
     desktop = smgr.createInstanceWithContext("com.sun.star.frame.Desktop", ctx)
     calc = desktop.getCurrentComponent()    
-    calc.storeAsURL('file://' + filename)
+    calc.storeAsURL('file://' + filename, ())
 
 def set_cell(sheet, row, col, data):
     xCell = sheet.getCellByPosition(col, row)
@@ -16,35 +16,6 @@ def set_cell(sheet, row, col, data):
             xCell.setValue(data)
         except: # ignore cells with invalid data
             print 'Invalid data ', data
-
-def saveSheetToFile(desktop, inFile, outFile, sheetName='Sheet1'):
-   
-   # Make a new document
-   document = desktop.loadComponentFromURL("private:factory/scalc", "_default", 0, ())
-   
-   # Add our new sheet
-   sheets = document.getSheets()
-   sheets.insertNewByName(sheetName, 0)
-   sheet = document.getSheets().getByName(sheetName)
-   sheet.link("file://" + inFile, sheetName, "calc8", "", NORMAL)
-   
-   # Break the link between the documents
-   sheet.setLinkMode(NONE)
-   
-   # Tidy up
-   removeDefaultSheets(document)
-   
-   # Save
-   extension = outFile.split(".")[-1:][0]
-   if extension == "csv":
-      args = toProperties({"ReadOnly":False, "FilterName":"Text - txt - csv (StarCalc)", "FilterOptions":"59,34,76,1"})
-   else:
-      args = toProperties({"ReadOnly":False})
-   
-   document.storeToURL("file://"+outFile, args)
-   
-   # Close
-   document.dispose()
 
 def calc2booktab():
         ctx = uno.getComponentContext()
@@ -66,9 +37,11 @@ def calc2booktab():
         cRange = range(fcol, lcol+1)
         rRange = range(frow, lrow+1)
 
-        # my simple test init
+        ###########################################
+        # Init for my simple test.
         out_col = 3
         out_row = 0
+        ###########################################
 
         for i in rRange:
                 newData = []
@@ -77,13 +50,14 @@ def calc2booktab():
                         seeCell = oCell.String
                         newData.append(seeCell)
                         
-                        # this is where we do my simple test
+                        ###########################################
+                        # This is where we do my simple test.
                         set_cell(sheet, out_row, out_col, seeCell)
                         out_row += 1
+                        ###########################################
                         
                 data_list.append(newData)
                 
-        #save_document('/tmp/trash.ods')
-        #saveSheetToFile(desktop, '/home/pims/Document/Untitled1.ods', '/tmp/trash.ods')
+        save_document( '/tmp/trashbigtime.ods' )
        
 g_exportedScripts = calc2booktab,
---
+++ office/readme.txt
@@ -0,0 +1,1 @@
+/home/pims/.config/libreoffice/3/user/Scripts/python -> /home/pims/dev/programs/python/office
