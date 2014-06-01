7e6141f58a48aba16456dbe1c5db0751b7ca2ae6
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2014-06-01 11:59:36 +0000

    Got new stub working with example file.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk/pims@633 ab604922-b16d-4620-a341-c5f183439689

--- excel/ledger.py
+++ excel/ledger.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python
 
+# TODO formulas on zin sheet to calculate deductions percentages of [after 401k]
+
 import os
 import datetime
 import shutil
 import xlwt
 import pandas as pd
-from xlsxwriter.utility import xl_rowcol_to_cell
+from xlsxwriter.utility import xl_rowcol_to_cell, xl_range
+
+sheets = {
+    'xactions': ['Date', 'Where', 'Delta', 'Note'],
+    'zin':      ['PayDate', 'Type', 'Hours', 'Amount'],
+    'vars':     ['Date', 'Variable', 'Value'],
+}
+
+def demo_add_row_to_dataframe(dfz):
+    new_row = pd.DataFrame([dict(PayDate=datetime.datetime.now().date(), Hours=80.0, Type='Regular', Amount=123.45), ])
+    dfz = dfz.append(new_row, ignore_index=True)   
+    print dfz[-5:]
 
 # return dict of dataframes, one for each sheet in Excel file
 def load_previous(file_name):
@@ -26,24 +39,8 @@ def load_previous(file_name):
     excel_file = pd.ExcelFile(file_name)
     dfs = {sheet_name: excel_file.parse(sheet_name) for sheet_name in excel_file.sheet_names}
 
-    #dfx = dfs['xactions']
-    #dfz = dfs['zin']
-    #dfv = dfs['vars']
-    #
-    #new_row = pd.DataFrame([dict(PayDate=datetime.datetime.now().date(), Hours=80.0, Type='Regular', Amount=123.45), ])
-    #dfz = dfz.append(new_row, ignore_index=True)
-    #
-    #print dfz[-5:]
-    #print dfv
-
     # Return dict of dataframes
     return dfs
-
-sheets = {
-    'xactions': ['Date', 'Where', 'Delta', 'Note'],
-    'zin':      ['PayDate', 'Type', 'Hours', 'Amount'],
-    'vars':     ['Date', 'Variable', 'Value'],
-}
 
 def add_pay_stub(file_name='/home/pims/Documents/ledger.xlsx'):
 
@@ -59,9 +56,9 @@ def add_pay_stub(file_name='/home/pims/Documents/ledger.xlsx'):
     writer = pd.ExcelWriter(file_name, engine='xlsxwriter')
 
     # Add formats to use
-    bold = writer.book.add_format({'bold': 1})
+    bold_format = writer.book.add_format({'bold': 1})
     date_format = writer.book.add_format({'num_format': 'dd-mmm-yyyy'})
-    money_format = writer.book.add_format({'num_format': '+#0.00;[RED]-#0.00;0'})
+    money_format = writer.book.add_format({'num_format': '+#0.00;[RED]-#0.00;#0.00'})
     right_align_format = writer.book.add_format({'align': 'right'})
     hour_format = writer.book.add_format({'num_format': '#0.0;[RED]-#0.0;0.0'})
 
@@ -69,20 +66,12 @@ def add_pay_stub(file_name='/home/pims/Documents/ledger.xlsx'):
     for sheet_name, column_order in sheets.iteritems():
         dfs[sheet_name].to_excel(writer, sheet_name=sheet_name, cols=column_order, index=False)
 
-    # TODO Create "new pay stub" entries (80 hours of Regular, est. deductions, etc.)
-    # TODO Use formulas for Amounts tied to Hours on zin sheet for easy manual changes
-    #      NOTE: these will get overwritten on next run with values instead of formulas
-    #
-    # dfs['vars'] # for MOST RECENT value of zin_hourly
-    # pay_date = previous + TWO_WEEKS
-    # somewhere around here we use write_formula method on zin sheet
-
     # New pay stub data
     dfz = dfs['zin']
     dfv = dfs['vars']
+    zin_hourly = max( dfv[ dfv.Variable == 'zin_hourly' ].Value )
     previous_date = max( dfz['PayDate'] )
-    new_date = previous_date + datetime.timedelta(days=14)
-    zin_hourly = max( dfv['Value'] ) # FIXME for nested index ['Variable']['Value']
+    new_date = previous_date + datetime.timedelta(days=14)   
     
     hour_items = (
         ['Regular',            80],
@@ -93,6 +82,7 @@ def add_pay_stub(file_name='/home/pims/Documents/ledger.xlsx'):
     # Start from the first cell below the last row
     row = len(dfs['zin']) + 1
     col = 0
+    first_row = row
     
     # Write hourly items (Regular, Holiday, Personal Leave)
     for my_type, hours in (hour_items):
@@ -104,17 +94,35 @@ def add_pay_stub(file_name='/home/pims/Documents/ledger.xlsx'):
         writer.sheets['zin'].write_formula(row, col + 3, amount_formula, money_format)
         row += 1
     
-    # Write deductions
+    # Write deductions & remaining rows by iteration
     dfprevious = dfz[dfz['PayDate'] == previous_date]
-    #dfdeductions = dfprevious[dfprevious['Amount'] < 0]
-    dfremain = dfprevious[ dfprevious.Type != 'Regular' ]
-    dfremain = dfremain[ dfremain.Type != 'Holiday' ]
-    dfremain = dfremain[ dfremain.Type != 'Personal Leave' ]
-    # TODO how to iterate over dfremain's "rows" to do "row writing" like above?
+    dfdeductions = dfprevious[dfprevious['Amount'] < 0]
+    #dfnonhourstuff = dfprevious[ dfprevious.Type != 'Regular' ]
+    #dfnonhourstuff = dfnonhourstuff[ dfnonhourstuff.Type != 'Holiday' ]
+    #dfnonhourstuff = dfnonhourstuff[ dfnonhourstuff.Type != 'Personal Leave' ]
+    
+    # iterate over row tuples of (idx, PayDate, Type, Hours, Amount)
+    for r in dfdeductions.iterrows():
+        #print r[1].PayDate, r[1].Type, r[1].Hours, r[1].Amount
+        writer.sheets['zin'].write_datetime(row, col + 0, new_date, date_format)
+        writer.sheets['zin'].write_string(row, col + 1, r[1].Type)
+        writer.sheets['zin'].write_number(row, col + 2, r[1].Hours, hour_format)
+        writer.sheets['zin'].write_number(row, col + 3, r[1].Amount, money_format)
+        row += 1
+    last_row = row - 1
+
+    # Write VERIFY ZERO SUM formula as last row of new pay stub
+    verify_cell_range = xl_range(first_row, 3, last_row, 3)
+    verify_formula = '=SUM(%s)' % verify_cell_range
+    writer.sheets['zin'].write_datetime(row, col + 0, new_date, date_format)
+    writer.sheets['zin'].write_string(row, col + 1, 'VERIFY ZERO SUM', bold_format)
+    writer.sheets['zin'].write_number(row, col + 2, 0.0, hour_format)
+    writer.sheets['zin'].write_formula(row, col + 3, verify_formula, money_format)
 
     # Set formats
     writer.sheets['zin'].set_column('A:A', 12, date_format)
     writer.sheets['zin'].set_column('B:B', 16, right_align_format)
+    writer.sheets['zin'].set_column('C:C', 9, hour_format)
     writer.sheets['zin'].set_column('D:D', 9, money_format)
     writer.sheets['xactions'].set_column('A:A', 12, date_format)
     writer.sheets['xactions'].set_column('B:B', 6, right_align_format)
