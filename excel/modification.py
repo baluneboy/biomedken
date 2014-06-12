2e4aee8ef498621d8355a0acc4f9e689ab745c2f
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2014-06-12 11:27:56 +0000

    Added special case to account for ossbtmf roadmap PDF files timestr with hours only (no mm_ss.sss).  Made patterns/dailyproducts.py _BATCHROADMAPS_PATTERN more robust to account for ossbtmf PDFs too.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk/pims@639 ab604922-b16d-4620-a341-c5f183439689

--- excel/modification.py
+++ excel/modification.py
@@ -2,10 +2,12 @@
 
 import calendar
 import datetime
+import numpy as np
 from openpyxl.workbook import Workbook
 from openpyxl.writer.excel import ExcelWriter
 from openpyxl.cell import Cell, get_column_letter, column_index_from_string, coordinate_from_string
 from openpyxl.reader.excel import load_workbook
+from openpyxl.style import Font, Color, NumberFormat, Style, Fill
 
 # OBSOLETE insert wall clock column [because pandas does it now]
 def OBSOLETE_insert_wall_clock_column(ws, left_of_column=2):
@@ -35,7 +37,6 @@ def OBSOLETE_insert_wall_clock_column(ws, left_of_column=2):
 # if month GMT range okay, then overwrite last row with per-column totals
 def reckon_month(ws):
     """if month GMT range okay, then overwrite last row with per-column totals"""
-    
     if ws.cell('A2').value == 'Date':
         gmt_start = ws.cell('A3').value
         if gmt_start.day == 1:
@@ -49,7 +50,7 @@ def reckon_month(ws):
                 #gmt_range_str = 'GMT range is %s through %s' % (gmt_start.strftime('%Y-%m-%d'), gmt_end.strftime('%Y-%m-%d'))
                 ws.cell(row=(last_row - 1), column=0).value = 'TOTAL'
                 for c in range(ws.get_highest_column())[1:]:
-                    letter = get_column_letter(c + 1)
+                    letter = get_column_letter(c + 1).upper()
                     formula_str = "=SUM(%s3:%s%d)" % (letter, letter, (last_row - 1))
                     ws.cell(row=(last_row - 1), column=c).value = formula_str
             else:
@@ -58,33 +59,95 @@ def reckon_month(ws):
             print 'gmt_start is not day one'
     else:
         print 'A2 is not Date'
-        
     return ws, gmt_start, gmt_end
 
+# get totals from column labels
+def get_totals_from_column_labels(xlsx_file, sheet_name, raw_column_labels):
+    wb = load_workbook(filename = xlsx_file)
+    ws = wb.get_sheet_by_name(sheet_name)    
+    #print '%s sheet %s has %d rows' % (xlsx_file, sheet_name, len(ws.rows))
+    last_row = len(ws.rows)
+    totals = {}
+    for idx, c in enumerate(raw_column_labels):
+        top_cell = ws.cell(row=0, column=idx+1)
+        bot_cell = ws.cell(row=last_row-1, column=idx+1)
+        #print top_cell.value, bot_cell.value
+        if top_cell.value == c:
+            totals[c] = bot_cell.value
+        else:
+            totals[c] = None
+    return totals
+
 # reckon GMT range and overwrite last row with totals, if okay
-def overwrite_last_row_with_totals(xlsxfile):
+def overwrite_last_row_with_totals(xlsx_file, df_config, bamf_df):
     """reckon GMT range and overwrite last row with totals, if okay"""
-    
+
     # load workbook and get "raw" and "kpi" worksheets
-    wb = load_workbook(filename = xlsxfile)
+    wb = load_workbook(filename = xlsx_file)
     ws = wb.get_sheet_by_name("raw")
     ws2 = wb.get_sheet_by_name("kpi")
     
     # check GMT range for month
     ws, gmt_start, gmt_end = reckon_month(ws)
-    ws.cell('A1').value = '/</-/'
+    _cell = ws.cell('A1')
+    _cell.value = '/</-/'
+
+     # keep only rows >= gmt_start and <= gmt_end
+    df_crux = bamf_df[ (bamf_df.index >= gmt_start.date()) & (bamf_df.index <= gmt_end.date()) ]
+    raw_sum = df_crux.sum() 
+ 
+    # iterate over rows to fill num, den, and formulas for kpi   
+    for r in range(2, len(df_config) + 2):
+        
+        # fill kpi formula cell value and style
+        formula_str = "=G%d/H%d" % (r, r)
+        dest_str = 'F%d' % r
+        _cell_kpi_formula = ws2.cell(dest_str)
+        _cell_kpi_formula.style.number_format.format_code = '#0.0%' 
+        _cell_kpi_formula.value = formula_str
+
+        ws2.cell('A' + str(r)).value = gmt_start
+        ws2.cell('B' + str(r)).value = gmt_end
+        numstr = ws2.cell('G' + str(r)).value
+        denstr = ws2.cell('H' + str(r)).value
+        
+        # Numerator
+        _cell_numerator = ws2.cell('G' + str(r))
+        try:
+            _cell_numerator.value = raw_sum[numstr]
+        except:
+            _cell_numerator.value = np.NaN
+        _cell_numerator.style.number_format.format_code = '#0.0'
+        
+        # Denominator
+        _cell_denominator = ws2.cell('H' + str(r))        
+        try:
+            _cell_denominator.value = raw_sum[denstr]
+        except:
+            _cell_denominator.value = np.NaN
+        _cell_denominator.style.number_format.format_code = '#0'
+            
+
+    ## Font properties
+    #_cell.style.font.color.index = Color.GREEN
+    #_cell.style.font.name = 'Arial'
+    #_cell.style.font.size = 8
+    #_cell.style.font.bold = True
+    #_cell.style.alignment.wrap_text = True
     
-    ws2.cell('A2').value = gmt_start
-    ws2.cell('B2').value = gmt_end
-    r = 2
-    formula_str = "=B%d-A%d" % (r, r)
-    ws2.cell('F2').value = formula_str
+    ## Cell background color
+    #_cell.style.fill.fill_type = Fill.FILL_SOLID
+    #_cell.style.fill.start_color.index = Color.DARKRED
     
+    ## You should only modify column dimensions after you have written a cell in 
+    ##     the column. Perfect world: write column dimensions once per column
+    #ws.column_dimensions["C"].width = 60.0    
+
     # save
-    wb.save(filename = xlsxfile)
+    wb.save(filename = xlsx_file)
 
 # kpi sheet fill
-def kpi_sheet_fill(xlsxfile, gmt_start, gmt_end):
+def kpi_sheet_fill(xlsx_file, gmt_start, gmt_end):
     """kpi sheet fill"""
     
     ############################################################################
@@ -122,7 +185,7 @@ def kpi_sheet_fill(xlsxfile, gmt_start, gmt_end):
     #    row += 1    
     
     # load workbook and get "raw" worksheet and "kpi" worksheet
-    wb = load_workbook(filename = xlsxfile)
+    wb = load_workbook(filename = xlsx_file)
     ws = wb.get_sheet_by_name("kpi")
     
     # 
@@ -130,7 +193,7 @@ def kpi_sheet_fill(xlsxfile, gmt_start, gmt_end):
     ws.cell('B2').value = gmt_end
     
     # save
-    wb.save(filename = xlsxfile)
+    wb.save(filename = xlsx_file)
     
     return gmt_start, gmt_end
 
@@ -188,3 +251,4 @@ def update_xlsx(src, dest):
 if __name__ == "__main__":
     #update_xlsx('/tmp/empty_book.xlsx', '/tmp/new_empty.xlsx')
     demo_open_existing()
+    
