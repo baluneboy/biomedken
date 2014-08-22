6a3d89ca5e4a3947543e59c8c915b6b669081aed
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2014-08-22 14:08:27 +0000

    Added sams/sublime/User/sort_lines_length.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk/pims@672 ab604922-b16d-4620-a341-c5f183439689

--- sandbox/redict_example.py
+++ sandbox/redict_example.py
@@ -50,30 +50,77 @@ def execute_method_for(s, mydict):
     for args, f in matches:
         return f(*args)
 
-mydict = {}
-mydict[re.compile('actionname (\d+) (\d+)')] = method1
-mydict[re.compile('differentaction (\w+) (\w+)')] = method2
-mydict[re.compile('.*/(syslog|syslog\.\d{1})')] = syslog_foldpat_list
-mydict[re.compile('.*/(messages|messages\.\d{1})')] = messages_foldpat_list
+def demo_fold():
+    mydict = {}
+    mydict[re.compile('actionname (\d+) (\d+)')] = method1
+    mydict[re.compile('differentaction (\w+) (\w+)')] = method2
+    mydict[re.compile('.*/(syslog|syslog\.\d{1})')] = syslog_foldpat_list
+    mydict[re.compile('.*/(messages|messages\.\d{1})')] = messages_foldpat_list
+    
+    out = execute_method_for('actionname 12 3', mydict)
+    out = execute_method_for('differentaction what up', mydict)
+    print out
+    
+    fname = '/home/pims/syslog'
+    out = execute_method_for(fname, mydict)
+    print out
+    
+    fname = '/home/sams-ii/messages.2'
+    out = execute_method_for(fname, mydict)
+    print out
+    
+    #red1 = RegExpDict(a='one', b='two')
+    #print red1
+    #
+    #keys_are_filenames = ["/home/pims/syslog", "/tmp/syslog.1", "c", "ab", "ce", "de"]
+    #vals_are_patlist = range(0,len(keys_are_filenames))
+    #red = RegExpDict(zip(keys_are_filenames, vals_are_patlist))
+    #
+    #for i in red[r".*/(syslog|syslog\.\d{1})"]:
+    #    print i
 
-out = execute_method_for('actionname 12 3', mydict)
-out = execute_method_for('differentaction what up', mydict)
-print out
+def tsh_accel_method(pth, tsh, clitype, four):
+    return 'tsh accel method', pth, tsh, clitype, four
 
-fname = '/home/pims/syslog'
-out = execute_method_for(fname, mydict)
-print out
+def tsh_state_method(pth, tsh, clitype, four, five):
+    return 'tsh state method', pth, tsh, clitype, four, five
 
-fname = '/home/sams-ii/messages.2'
-out = execute_method_for(fname, mydict)
-print out
+def non_tsh_method(pth, dev, ee, port):
+    return 'non-tsh method', pth, dev, ee, port
 
-#red1 = RegExpDict(a='one', b='two')
-#print red1
-#
-#keys_are_filenames = ["/home/pims/syslog", "/tmp/syslog.1", "c", "ab", "ce", "de"]
-#vals_are_patlist = range(0,len(keys_are_filenames))
-#red = RegExpDict(zip(keys_are_filenames, vals_are_patlist))
-#
-#for i in red[r".*/(syslog|syslog\.\d{1})"]:
-#    print i
+gencli_dict = {}
+gencli_dict[re.compile('(.*)generic_client (.*) (.*) (.*)')] = non_tsh_method
+gencli_dict[re.compile('(.*)generic_client (tshes-\d+)-accel (.*) (.*)')] = tsh_accel_method
+gencli_dict[re.compile('(.*)generic_client (tshes-\d+)-state (.*) (.*) (.*)')] = tsh_state_method
+
+def do_something(s, gencli_dict):
+    # Match each regex on the string
+    matches = (
+        (regex.match(s), f) for regex, f in gencli_dict.iteritems()
+    )
+
+    # Filter out empty matches, and extract groups
+    matches = (
+        (match.groups(), f) for match, f in matches if match is not None
+    )
+
+    # Apply all the functions
+    for args, f in matches:
+        return f(*args)
+   
+def demo_gen_cli():
+    
+    for cmd in [
+        'generic_client 121-f02 ee122-f04 9803',
+        'generic_client 122-f04 ee122-f04 9805',
+        '/usr/local/bin/generic_client 121-f02 ee122-f04 9803',
+        '/usr/local/bin/generic_client 122-f04 ee122-f04 9805',
+        '/usr/local/bin/generic_client tshes-05-accel tshes-05 9760',
+        '/usr/local/bin/generic_client tshes-06-state tshes-06 9761 70',
+        ]:
+        out = do_something(cmd, gencli_dict)
+        print [i for i in out]
+    
+if __name__ == "__main__":
+    #demo_fold()
+    demo_gen_cli()
