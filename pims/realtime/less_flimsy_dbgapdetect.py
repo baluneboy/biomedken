093010afa03ecb5776b84766425b76c4e3abf3b3
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2014-09-08 18:56:32 +0000

    Check-in latest realtime/less_flimsy_dbgapdetect.py and database/samsquery.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@686 ab604922-b16d-4620-a341-c5f183439689

--- pims/realtime/less_flimsy_dbgapdetect.py
+++ pims/realtime/less_flimsy_dbgapdetect.py
@@ -3,10 +3,13 @@ version = '$Id$'
 
 import os
 import sys
+import socket
 import datetime
 from pims.database.pimsquery import db_connect, mysql_con
 import pandas.io.sql as psql
+import pandas as pd
 import MySQLdb
+from cStringIO import StringIO
 
 # TODO
 # - robustness for missing table (or go out and find hosts/tables automagically)
@@ -38,7 +41,7 @@ defaults = {
     ],          
 'packets_per_sec':  '8',    # expected value for this sensor for this gap check period
 'min_pct':          '0',    # show hourly periods with pkt count < min_pct (USE ZERO TO SHOW ALL)
-'hours_ago':        '23',   # start checking this many hours ago
+'hours_ago':        '15',   # start checking this many hours ago
 }
 parameters = defaults.copy()
 
@@ -92,8 +95,8 @@ class DatabaseGaps(object):
         query =  'SELECT FROM_UNIXTIME(time) as "hour", '
         #query += 'ROUND(100*COUNT(*)/8.0/3600.0) as "pct", '
         #query += 'COUNT(*) as "pkts" from %s ' % self.sensor
-        query += 'ROUND(100*COUNT(*)/8.0/3600.0) as "%s pct", ' % self.sensor
-        query += 'COUNT(*) as "%s pkts" from %s ' % (self.sensor, self.sensor)
+        query += 'ROUND(100*COUNT(*)/8.0/3600.0) as "%s<br>%%", ' % self.sensor
+        query += 'COUNT(*) as "%s<br>pkts" from %s ' % (self.sensor, self.sensor)
         query += 'WHERE FROM_UNIXTIME(time) >= "%s" ' % self.start.strftime('%Y-%m-%d %H:%M:%S')
         query += 'AND FROM_UNIXTIME(time) < "%s" ' % self.stop.strftime('%Y-%m-%d %H:%M:%S')
         query += "GROUP BY DATE_FORMAT(FROM_UNIXTIME(time), '%H') ORDER BY time;"
@@ -112,6 +115,152 @@ class DatabaseGaps(object):
     def filter(self, predicate):
         """return filtered dataframe"""
         pass
+
+# function to format percentages
+def percentage_fmt(x):
+    """function to format percentages"""
+    if x < 50:               s = '<span style="color: red">%.1f</span>' % x
+    elif x >= 50 and x < 95: s = '<span style="color: blue;">%.1f</span>' % x
+    else:                    s = '%.1f' % x
+    return s
+
+# FIXME this time conversion does not work (see example in samsquery.py)
+# function to format hourlies
+def hourly_fmt(x):
+    """function to format hourlies"""
+    d = pd.to_datetime(x)
+    all_balls = d.minute == 0 and d.second == 0 and d.microsecond == 0
+    if all_balls: s = '%s' % x
+    else:         s = '<span style="color: red;">%s</span>' % x
+    return s
+
+"""NOTE THIS GREEN COMMENT PART IS SNIPPET OF POSSIBLY IMPROVED TABLE HEADER ROW
+<SNIP>
+}
+.df tbody tr:nth-child(even) {background: #CCC} tr:nth-child(odd) {background: #FFF}
+</style>
+</head>
+<body>
+<titletag>PIMS Database Tables</titletag><br>
+<updatetag>updated at GMT 2014-09-06 10:11:03</updatetag><br>
+<hosttag>host: jimmy</hosttag><br>
+<br>
+<table class="dataframe df" border="1">
+<thead> <tr>
+<th><toprow>LAB/RACK</toprow></th>
+<th colspan="2"><toprow>COL/ER3</toprow></th>
+<th colspan="2"><toprow>USL/ER2</toprow></th>
+<th colspan="2"><toprow>USL/ER1</toprow></th>
+<th colspan="2"><toprow>JEM/ER4</toprow></th>
+<th colspan="2"><toprow>COL/ER3</toprow></th>
+<th colspan="2"><toprow>USL/FIR</toprow></th>
+</tr>
+<tr>
+<th>hour</th>
+<th><dbhosttag>kenny</dbhosttag><br>121f02<br>
+~data%</th>
+<th><dbhosttag>kenny</dbhosttag><br>121f02<br>
+#pkts</th>
+<th><dbhosttag>tweek</dbhosttag><br>121f03<br>
+~data%</th>
+<th><dbhosttag>tweek</dbhosttag><br>121f03<br>
+#pkts</th>
+<th><dbhosttag>mr-hankey</dbhosttag><br>121f04<br>
+~data%</th>
+<th><dbhosttag>mr-hankey</dbhosttag><br>121f04<br>
+#pkts</th>
+<th><dbhosttag>chef</dbhosttag><br>121f05<br>
+~data%</th>
+<th><dbhosttag>chef</dbhosttag><br>121f05<br>
+#pkts</th>
+<th><dbhosttag>timmeh</dbhosttag><br>121f08<br>
+~data%</th>
+<th><dbhosttag>timmeh</dbhosttag><br>121f08<br>
+#pkts</th>
+<th><dbhosttag>manbearpig</dbhosttag><br>es06<br>
+~data%</th>
+<th><dbhosttag>manbearpig</dbhosttag><br>es06<br>
+#pkts</th>
+</tr>
+</thead> <tbody>
+<SNIP>
+"""
+
+HEADER = '''<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="refresh" content="600">
+<title>PIMS db</title>
+<style>
+updatetag {
+text-align: left;
+color: black;
+font-size: 0.83em;
+font-family: Verdana,Geneva,sans-serif;
+}
+dbhosttag {
+font-size: 0.55em;
+text-align: right;
+font-family: Verdana,Geneva,sans-serif;
+color: gray;
+}
+hosttag {
+font-size: 0.75em;
+text-align: left;
+font-family: Verdana,Geneva,sans-serif;
+color: gray;
+}
+titletag {
+font-size: 1.25em;
+font-weight: bold;
+font-family: Verdana,Geneva,sans-serif;
+color: black;
+text-align: left;
+text-decoration: underline;
+}
+captiontag {
+font-size: 1.1em;
+font-weight: bold;
+font-family: Verdana,Geneva,sans-serif;
+color: black;
+text-align: left;
+}
+toprow {
+background-color: black;
+font-size: 0.729em;
+color: orange;
+}
+.df {
+width: 100%;
+font-family: Verdana,Geneva,sans-serif;
+border-collapse: collapse;
+}
+.df td, .df th {
+border: 1px solid #123456;
+padding: 3px 7px 2px;
+font-size: 0.99em;
+}
+.df th {
+border: 1px solid #ffffff;
+background-color: black;
+font-size: 1.1em;
+vertical-align: bottom;
+padding-bottom: 5px;
+padding-top: 5px;
+color: #ffffff;
+}
+.df tbody tr:nth-child(even) {background: #CCC} tr:nth-child(odd) {background: #FFF}
+</style>
+</head>
+<body>
+<titletag>PIMS Database Tables</titletag><br>
+        '''
+FOOTER = '<updatetag>updated at GMT %s</updatetag><br>' % str(datetime.datetime.now())[0:-7]
+FOOTER += '<hosttag>host: %s</hosttag><br><br>' % socket.gethostname()
+FOOTER += '''
+    </body>
+</html>
+'''
 
 def params_okay():
     """Not really checking for reasonableness of parameters entered on command line."""
@@ -149,9 +298,13 @@ def main(argv):
             parameters[pair[0]] = pair[1]
     else:
         if params_okay():
-            
+
+            buf = StringIO()
+            df_merged = pd.DataFrame({'hour':[]})
+            df_formatters = dict()
             for sensor, host in parameters['sensorhosts']:
                 print sensor, host
+                df_formatters['%s<br>%%' % sensor] = percentage_fmt
                 try:
                     # first, get all info on gaps
                     dbgaps = DatabaseGaps(
@@ -165,12 +318,22 @@ def main(argv):
                     # filter using min_pct
                     df_gaps = dbgaps.filt_min_pct()
                     # get result into string
-                    msg = df_gaps.to_string(formatters=DF_FORMATTERS, index=False)
+                    msg = df_gaps.to_string(formatters=dbgaps.formatters, index=False)
+                    df_merged = pd.merge(df_merged, df_gaps, how='outer')
     
                 except Exception as e:
-                    msg = "Exception %s" % e.message
+                    msg = "Exception %s" % e[1]
     
-                print msg or 'done'
+                #print msg or 'done'
+            
+            df_merged.sort(columns=['hour'], inplace=True)
+            df_merged.to_html(buf, formatters=df_formatters, escape=False, index=False, na_rep='nan')
+            s = buf.getvalue()
+            s = s.replace('<tr>', '<tr style="text-align: right;">')
+            with open("/misc/yoda/www/plots/user/sams/dbpims.html", "w") as html_file:
+                html_file.write( HEADER + s.replace('nan', '') + FOOTER )            
+            #print df_merged
+            
             return 0
 
     print_usage()  
