8b98ef87a2698319ddc6883a29ef806d184c7612
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2014-09-13 14:54:41 +0000

    At home improvements on sandbox/grand_unified.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk/pims@690 ab604922-b16d-4620-a341-c5f183439689

--- sandbox/grand_unified.py
+++ sandbox/grand_unified.py
@@ -15,17 +15,41 @@ def parse(s):
     d = datetime.date(yr, mo, da)
     return d
 
+# getr rid of NaN and dash in timestr
+def replace_timestr(t):
+    """getr rid of NaN and dash in timestr"""
+    if isinstance(t, float):
+        return None
+    if '-' == t:
+        return None
+    return t
+
 # read tab-delimited file (not csv, because some cells have commas)
-tab_file = '/home/pims/Downloads/scratch.csv'
+tab_file = '/home/pims/dev/programs/python/pims/sandbox/data/scratch.csv'
 df = pd.read_csv(tab_file, sep='\t')
 
 # get rid of rows that have NaN as a value for "set"
 df = df[ ~np.isnan(df['set']) ]
 df['gmt'] = df['date'].map(parse)
 
-# group by tag to get unique tags
-g = df.groupby(['gmt', 'tag'])
-for utag, dfss in iter(g):
-    print utag
-    print dfss
-    print '-' * 33
+# fix timestr (mostly, but still a string)
+df['range'] = df['Maneuver Start-Stop GMT'].map(replace_timestr)
+
+# group by tag to get/work on unique tags
+grp = df.groupby(['gmt', 'tag'])
+for tag, dfss in iter(grp):
+    print tag[0], tag[1]
+    
+    # group by set number to get/work set-by-set
+    setgrp = dfss.groupby('set')
+    for s, dfset in iter(setgrp):
+        #print dfset
+        print 'set %04d' % s,
+        ypr = [ i for i in dfset['YPR'] ]
+        print np.round(ypr, decimals=1),
+        times = [ i for i in dfset['range'] ]
+        print times,
+        events = [ i for i in dfset['Event'] ]
+        print events
+
+    print '=' * 44
