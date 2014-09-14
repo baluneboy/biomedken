4a9d71303d721ff8f25f5bc0ce4d806a66d73b59
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2014-09-14 12:11:18 +0000

    Chip away at home on sandbox/grand_unified.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@691 ab604922-b16d-4620-a341-c5f183439689

--- pims/sandbox/grand_unified.py
+++ pims/sandbox/grand_unified.py
@@ -15,7 +15,7 @@ def parse(s):
     d = datetime.date(yr, mo, da)
     return d
 
-# getr rid of NaN and dash in timestr
+# get rid of NaN and dash in timestr
 def replace_timestr(t):
     """getr rid of NaN and dash in timestr"""
     if isinstance(t, float):
@@ -24,8 +24,16 @@ def replace_timestr(t):
         return None
     return t
 
+repo = (('framework', 'django', '1.3'), ('cms', 'fein', '1.3'), ('cms', 'django-cms', '2.2'))
+types = {'desc': 0, 'name': 1, 'version': 2}
+def repo_filter(mytype, critera, repo=repo, types=types):
+    return [x for x in repo if x[types[mytype]] == critera]
+print repo_filter('version', '1.3')
+raise SystemExit
+
 # read tab-delimited file (not csv, because some cells have commas)
 tab_file = '/home/pims/dev/programs/python/pims/sandbox/data/scratch.csv'
+tab_file = '/home/pims/Downloads/Grand_Unified_ATL.csv'
 df = pd.read_csv(tab_file, sep='\t')
 
 # get rid of rows that have NaN as a value for "set"
@@ -38,18 +46,28 @@ df['range'] = df['Maneuver Start-Stop GMT'].map(replace_timestr)
 # group by tag to get/work on unique tags
 grp = df.groupby(['gmt', 'tag'])
 for tag, dfss in iter(grp):
-    print tag[0], tag[1]
+    datetag, tag = tag
+    print '%s: %s' % (datetag, tag)
     
     # group by set number to get/work set-by-set
     setgrp = dfss.groupby('set')
     for s, dfset in iter(setgrp):
         #print dfset
         print 'set %04d' % s,
-        ypr = [ i for i in dfset['YPR'] ]
-        print np.round(ypr, decimals=1),
-        times = [ i for i in dfset['range'] ]
-        print times,
-        events = [ i for i in dfset['Event'] ]
-        print events
+        
+        #ypr = [ i for i in dfset['YPR'] ]
+        #print ', ypr:', np.round(ypr, decimals=1),
+        #
+        #times = [ i for i in dfset['range'] ]
+        #print ', time(s):', times,
+        #
+        #events = [ i for i in dfset['Event'] ]
+        #print ', evts:', events,
+        #
+        #remarks = [ i for i in dfset['Remarks'] ]
+        #print ', remarks:', remarks
+        
+        print ', remarks:', clean_trio(dfset['Remarks'])
+        
 
     print '=' * 44
