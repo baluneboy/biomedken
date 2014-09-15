3e3b78fc64abb187a3e8717a159190f0ae15a289
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2014-09-15 18:46:25 +0000

    Big upgrade to sandbox/grand_unified.py.  Minor, nicer comment in utils/pimsdateutil.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@692 ab604922-b16d-4620-a341-c5f183439689

--- pims/utils/pimsdateutil.py
+++ pims/utils/pimsdateutil.py
@@ -87,7 +87,11 @@ def timestr_to_datetime(timestr):
 
 # convert string like 2014:077:00:02:00 to datetime object
 def doytimestr_to_datetime(timestr):
-    """convert string like 2014:077:00:02:00 to datetime object"""
+    """convert string like 2014:077:00:02:00 to datetime object
+    
+    >>> doytimestr_to_datetime('2014:077:00:02:00')
+    datetime.datetime(2014, 3, 18, 0, 2)
+    """    
     if not re.match('^\d{4}:\d{3}:\d{2}:\d{2}:(\d{2}\.\d{1,6}|\d{2})$', timestr):
         raise ValueError('string does not match expected pattern')
     if '.' in timestr:
