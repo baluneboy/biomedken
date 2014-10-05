c69c519969e2dd5d4a304c761171f406a4304eae
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2014-10-05 12:23:12 +0000

    Turned example code into StringBufferPackage class.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@698 ab604922-b16d-4620-a341-c5f183439689

--- pims/sandbox/padhex_example.py
+++ pims/sandbox/padhex_example.py
@@ -16,19 +16,46 @@ ENDIANNESS = {
     'unspecified'     : '',
     }
 
+class StringBufferPackage(object):
+    
+    def __init__(self, fmt, values, en='network'):
+        self.byteorder = en
+        self.format = ENDIANNESS[en] + ' ' + fmt
+        self.values = values
+        self.struct = struct.Struct(self.format)
+        self.strbuf = ctypes.create_string_buffer(self.struct.size)
+        self.ispacked = False
+        
+    def packinto(self):
+        self.struct.pack_into(self.strbuf, 0, *self.values)
+        self.ispacked = True
+    
+    def __str__(self):
+        s = '-'*33 + '\n'
+        s += 'Format    : "%s" (endianness prefix "%s" for %s byte ordering)\n' % (self.format, ENDIANNESS[self.byteorder], self.byteorder)
+        if self.ispacked:
+            s += 'AfterPack : %s (hex fmt packed-into buffer with "%s" byte ordering)\n' % (hexlify(self.strbuf.raw), self.byteorder)
+            s += 'Original  : %s\n' % str(self.values)
+            s += 'Unpacked  : %s' % str(self.struct.unpack_from(self.strbuf, 0))
+        else:
+            s += 'BeforePack: %s (hex fmt pre-allocated, empty buffer)' % hexlify(self.strbuf.raw)
+        s += '\n' + '-'*66
+        return s
+    
 def show_pack(fmt, values, en='network'):
     fmt = ENDIANNESS[en] + ' ' + fmt
     s = struct.Struct(fmt)
     print 'Format    : "%s" (endianness prefix "%s" for %s byte ordering)' % (fmt, ENDIANNESS[en], en)
     b = ctypes.create_string_buffer(s.size)
-    print 'Before    :', hexlify(b.raw), '(hex fmt pre-allocated, empty buffer)'
+    print 'BeforePack:', hexlify(b.raw), '(hex fmt pre-allocated, empty buffer)'
     s.pack_into(b, 0, *values)
-    print 'After     :', hexlify(b.raw), '(hex fmt packed-into buffer with "%s" byte ordering)' % en
+    print 'AfterPack :', hexlify(b.raw), '(hex fmt packed-into buffer with "%s" byte ordering)' % en
     print 'Original  :', values
     print 'Unpacked  :', s.unpack_from(b, 0)
     return b
 
 def demo_pack_unpack(ut, endianness='network'):
+
     print 'human time: %s (ORIGINAL)' % UnixToHumanTime(ut)
     fracsec, sec = math.modf(ut)
     usec = fracsec*1000000.0
@@ -36,12 +63,20 @@ def demo_pack_unpack(ut, endianness='network'):
     print 'ut part #1: %s' % format(int(sec), '#034b'),  # 32 bits  + 2 placeholders for "0b" prefix
     print '(hex: %s for sec part)' % format(int(sec), '#x')           #  8 bytes + 2 placeholders for "0x" prefix
     print 'ut part #2: %s' % format(int(usec), '#034b'), # 32 bits  + 2 placeholders for "0b" prefix
-    print '(hex: %s for usec part)' % format(int(usec), '#010x')       #  8 bytes + 2 placeholders for "0x" prefix
-    b = show_pack('II', (int(sec), int(usec)), en=endianness) # 2nd arg for values is always a tuple
+    print '(hex: %s for usec part)' % format(int(usec), '#010x')       #  8 bytes + 2 placeholders for "0x" prefix   
+
+    # old demo way to do this
+    #b = show_pack('II', (int(sec), int(usec)), en=endianness) # 2nd arg for values is always a tuple
+    
+    # now do same thing with class way    
+    sb = StringBufferPackage('II', (int(sec), int(usec)), en=endianness) # 2nd arg for values is always a tuple
+    print sb
+    sb.packinto()
+    print sb
     
     # verify that b works like what is expected in Ted's code
     en_prefix = ENDIANNESS[endianness]
-    sec1, usec1 = struct.unpack(en_prefix + 'II', b) # endianness refix to specify byte ordering of unpack
+    sec1, usec1 = struct.unpack(en_prefix + 'II', sb.strbuf) # endianness refix to specify byte ordering of unpack
     print 'human time: %s (ORIGINAL)' % UnixToHumanTime(ut)
     print 'human time: %s (AFTER PACK-UNPACK)' % UnixToHumanTime( sec1 + usec1/1000000.0 )
 
