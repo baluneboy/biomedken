9c3ad709f5b9ecfe14f0665c7090648b6628df65
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2014-10-27 13:49:02 +0000

    Added sandbox/aifc_example.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@721 ab604922-b16d-4620-a341-c5f183439689

---
+++ pims/sandbox/aifc_example.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import aifc
+
+if __name__ == '__main__':
+    fn = '/home/pims/Downloads/wood12.aiff'
+    f = aifc.open(fn, 'r')
+    print "Reading", fn
+    print "nchannels =", f.getnchannels()
+    print "nframes   =", f.getnframes()
+    print "sampwidth =", f.getsampwidth()
+    print "framerate =", f.getframerate()
+    print "comptype  =", f.getcomptype()
+    print "compname  =", f.getcompname()
+    gn = '/home/pims/Downloads/wood12_out.aiff'
+    print "Writing", gn
+    g = aifc.open(gn, 'w')
+    g.setparams(f.getparams())
+    while 1:
+        data = f.readframes(1024)
+        if not data:
+            break
+        g.writeframes(data)
+    g.close()
+    f.close()
+    print "Done."
