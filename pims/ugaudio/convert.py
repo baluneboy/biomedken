98716c4aa5e937f2534b90e876ac850a52351805
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2014-11-03 19:24:22 +0000

    Removed ugaudio/settings.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@745 ab604922-b16d-4620-a341-c5f183439689

--- pims/ugaudio/convert.py
+++ pims/ugaudio/convert.py
@@ -17,7 +17,7 @@ from pims.ugaudio.signal import normalize
 import matplotlib.pyplot as plt
 
 # convert designated axis of input file to aiff
-def convert(filename, samplerate=None, axis='z', plot=False):
+def convert(filename, samplerate=None, axis='s', plot=False):
     """convert designated axis of input file to aiff"""
 
     # use loose interpreation of what a PAD file is here
@@ -45,37 +45,37 @@ def convert(filename, samplerate=None, axis='z', plot=False):
     # FIXME how to use numpy when axis is 's' for 'sum'
     
     # determine axis
-    ax = axis.lower()
-    if ax == 'x':   data = C[:, -3] # x-axis is 3rd last column
-    elif ax == 'y': data = C[:, -2] # y-axis is 2nd last column
-    elif ax == 'z': data = C[:, -1] # z-axis is the last column
-    elif ax == 's': data = C[:, 1::].sum(axis=1) # sum(x+y+z)
-    else:
-        print 'unhandled axis "%s", so just use z-axis' % axis
-        ax = 'z'
-        data = C[:, -1] # just use z-axis in this case
-
-    # plot demeaned accel data (if plot is to be produced)
-    if plot:
-        png_file = filename + ax + '.png'
-        plt.plot(data)
-        plt.savefig(png_file)
-        print 'wrote accel plot %s' % png_file
-        
-    # normalize to range -32768:32767 (actually, use -32000:32000)
-    data = normalize(data) * 32000.0
-
-    # data conditioning
-    data = data.astype(np.int16) # not sure why we need this...maybe aifc assumptions
-    data = data.byteswap().newbyteorder() # need this on mac osx and linux (windows?)
-
-    # convert data to string for aifc to work write
-    strdata = data.tostring()
-    aiff_file = filename + ax + '.aiff'
-    g = aifc.open(aiff_file, 'w')
-    sampwidth = 2
-    #         nchans, sampwidth, framerate, nframes, comptype, compname
-    g.setparams((1, sampwidth, samplerate, len(data), 'NONE', 'not compressed'))
-    g.writeframes(strdata)
-    g.close()
-    print 'wrote sound file %s' % aiff_file
+    for ax in axis.lower():
+        if ax == 'x':   data = C[:, -3] # x-axis is 3rd last column
+        elif ax == 'y': data = C[:, -2] # y-axis is 2nd last column
+        elif ax == 'z': data = C[:, -1] # z-axis is the last column
+        elif ax == 's': data = C[:, 1::].sum(axis=1) # sum(x+y+z)
+        else:
+            print 'unhandled axis "%s", so just use z-axis' % axis
+            ax = 'z'
+            data = C[:, -1] # just use z-axis in this case
+    
+        # plot demeaned accel data (if plot is to be produced)
+        if plot:
+            png_file = filename + ax + '.png'
+            plt.plot(data)
+            plt.savefig(png_file)
+            print 'wrote accel plot %s' % png_file
+            
+        # normalize to range -32768:32767 (actually, use -32000:32000)
+        data = normalize(data) * 32000.0
+    
+        # data conditioning
+        data = data.astype(np.int16) # not sure why we need this...maybe aifc assumptions
+        data = data.byteswap().newbyteorder() # need this on mac osx and linux (windows?)
+    
+        # convert data to string for aifc to work write
+        strdata = data.tostring()
+        aiff_file = filename + ax + '.aiff'
+        g = aifc.open(aiff_file, 'w')
+        sampwidth = 2
+        #         nchans, sampwidth, framerate, nframes, comptype, compname
+        g.setparams((1, sampwidth, samplerate, len(data), 'NONE', 'not compressed'))
+        g.writeframes(strdata)
+        g.close()
+        print 'wrote sound file %s' % aiff_file
--- pims/ugaudio/ugaudio.py
+++ pims/ugaudio/ugaudio.py
@@ -12,7 +12,7 @@ Given zero input arguments, this program shows this help text and quits.
 
 Given multiple input arguments, this program attempts to read each as an
 acceleration (PAD) data file named <filename> and convert its contents to an
-AIFF audio file named <filenameX.aiff>; where X designates which axis.
+AIFF audio file with suffix "s.aiff"; where s designates sum(x+y+z) axis data.
 
 Given the input argument "demo" [no quotes], this program generates its own
 (fake) acceleration (PAD) data and treats that as it would given one input
@@ -20,23 +20,22 @@ argument, which is described below.
 
 Given one input argument, this program takes that as an acceleration (PAD) data
 file named <filename> and does the following with its contents:
-(1) produce plot of the demeaned acceleration data <filenameX.png>
-(2) write AIFF audio file named <filenameX.aiff>; where X designates which axis
-
-User settings are in settings.py. You should probably SAVE THE ORIGINAL VERSION
-of the settings.py file in case something gets messed up.
+(1) produce 4 plots of the demeaned acceleration data:
+    1. <filenamex.png> for X-axis
+    2. <filenamey.png> for Y-axis
+    3. <filenamez.png> for Z-axis
+    4. <filenames.png> for sum(x+y+z)
+(2) write 4 AIFF audio files named similar to the PNG plot files above except with
+    the extension "aiff" instead of "png"
 """
 
 # Author: Ken Hrovat
 # Disclaimer: this project deserves more time than I am able to give it
 # FIXME - this code should be much more graceful in handling the unexpected
 
-# TODO settings.py for samplerate, axis, and plot defaults and overrides
-
 import sys
 from pims.ugaudio.demo import demo_chirp
 from pims.ugaudio.convert import convert
-from pims.ugaudio.settings import params_ok, parameters
 
 # get inputs and run
 def main():
@@ -45,10 +44,6 @@ def main():
     # no input args, so just show doc
     if not sys.argv[1:]:
         print __doc__
-    
-    # verify parameters
-    elif not params_ok():
-        print 'bad parameter(s), so exit'
     
     # demo
     elif sys.argv[1].lower() == 'demo':
@@ -59,13 +54,13 @@ def main():
     elif len(sys.argv) == 2:
         print 'one file mode'
         filename = sys.argv[1]
-        convert(filename, samplerate=None, axis='s', plot=True)
+        convert(filename, samplerate=None, axis='xyzs', plot=True)
     
     # batch files mode: only sound file output for each input file
     else:
         print 'batch files mode'
         for filename in sys.argv[1:]:
-            convert(filename, samplerate=None, axis='s', plot=False)
+            convert(filename)
     
     sys.exit(0)
 
