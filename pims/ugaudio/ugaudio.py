56a365184ee2f89551e7de63e077837d1d88f8aa
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2014-11-04 15:39:44 +0000

    Added ugaudio/matlab/readASCOTCalib.m (unrelated).
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@749 ab604922-b16d-4620-a341-c5f183439689

--- pims/ugaudio/ugaudio.py
+++ pims/ugaudio/ugaudio.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 
 """
-USAGE: ugaudio [ demo | <filename> | <filenames> ]
+For some important considerations and disclaimers, see the readme.txt file.
 
-SEE the readme.txt file for some important considerations and disclaimers.
-
-This simple program attempts to convert acceleration data files (PAD files) into
-AIFF audio files.
+This program attempts to convert PIMS acceleration data (PAD) files into AIFF
+audio files, and possibly plot the demeaned acceleration data too (see below).
 
 Given zero input arguments, this program shows this help text and quits.
 
@@ -34,37 +32,143 @@ file named <filename> and does the following with its contents:
 # FIXME - this code should be much more graceful in handling the unexpected
 
 import sys
+import argparse
 from pims.ugaudio.pad import PadFile
-from pims.ugaudio.demo import demo_chirp
-from pims.ugaudio.convert import convert
+#from pims.ugaudio.demo import demo_chirp
+#from pims.ugaudio.convert import convert
 
+# /misc/yoda/pub/pad/year2014/month10/day09/sams2_accel_121f04/2014_10_09_13_32_23.772+2014_10_09_13_34_02.623.121f04
+
+# class to override argparse error message
+class MyParser(argparse.ArgumentParser):
+    """class to override argparse error message"""
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit(2)
+
+# parse input arguments
+def OLDparse_args():
+    """parse input arguments"""
+
+    parser = MyParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    subparsers = parser.add_subparsers(help='commands')
+
+    # a demo command
+    demo_parser = subparsers.add_parser('demo', help='Demo')
+    demo_parser.add_argument('demo_name', action='store', help='Which demo to do {chirp,voice}')
+
+    # a list command
+    list_parser = subparsers.add_parser('list', help='List contents')
+    list_parser.add_argument('dirname', action='store', help='Directory to list')
+
+    # a create command
+    create_parser = subparsers.add_parser('create', help='Create a directory')
+    create_parser.add_argument('dirname', action='store', help='New directory to create')
+    create_parser.add_argument('--read-only', default=False, action='store_true',
+                               help='Set permissions to prevent writing to the directory',
+                               )
+
+    # a delete command
+    delete_parser = subparsers.add_parser('delete', help='Remove a directory')
+    delete_parser.add_argument('dirname', action='store', help='The directory to remove')
+    delete_parser.add_argument('--recursive', '-r', default=False, action='store_true',
+                               help='Remove the contents of the directory, too',
+                               )
+
+    # no input args, so just show doc help
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    args = vars( parser.parse_args() )
+    return args
+
+def TOOparse_args():
+    parser = MyParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    
+    parser.add_argument('-s', action='store', dest='simple_value',
+                        help='Store a simple value')
+    
+    parser.add_argument('-c', action='store_const', dest='constant_value',
+                        const='value-to-store',
+                        help='Store a constant value')
+
+    parser.add_argument('-d', action='store_true', default=False,
+                        dest='demo_switch',
+                        help='Set demo switch to true')
+    
+    parser.add_argument('-t', action='store_true', default=False,
+                        dest='boolean_switch',
+                        help='Set a switch to true')
+    parser.add_argument('-f', action='store_false', default=False,
+                        dest='boolean_switch',
+                        help='Set a switch to false')
+    
+    parser.add_argument('-a', action='append', dest='collection',
+                        default=[],
+                        help='Add repeated values to a list',
+                        )
+    
+    parser.add_argument('-A', action='append_const', dest='const_collection',
+                        const='value-1-to-append',
+                        default=[],
+                        help='Add different values to list')
+    parser.add_argument('-B', action='append_const', dest='const_collection',
+                        const='value-2-to-append',
+                        help='Add different values to list')
+    
+    parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+    
+    results = parser.parse_args()
+    print 'simple_value     =', results.simple_value
+    print 'constant_value   =', results.constant_value
+    print 'demo_switch      =', results.demo_switch
+    print 'boolean_switch   =', results.boolean_switch
+    print 'collection       =', results.collection
+    print 'const_collection =', results.const_collection
+
+def parse_args():
+    parser = MyParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+        
+    parser.add_argument('-s', default="closed", choices=['closed', 'wontfix', 'notabug'], help="bug status")
+     
+    parser.add_argument('bugnum', type=int, help="bug number to be statused")
+     
+    parser.add_argument('message', nargs='*', help="optional message")
+     
+    args = parser.parse_args()
+     
+    print("~ Bug Num: {}".format(args.bugnum))
+    print("~ Status : {}".format(args.s))
+    print("~ Message: {}".format(" ".join(args.message)))    
+    
 # get inputs and run
 def main():
     """get inputs and run"""
-    
-    # no input args, so just show doc
-    if not sys.argv[1:]:
-        print __doc__
-    
+
+    args = parse_args()
+    raise SystemExit
+
     # demo
-    elif sys.argv[1].lower() == 'demo':
+    if sys.argv[1].lower() == 'demo':
         print 'demo mode'
         demo_chirp()
-    
+
     # one file mode: both sound and plot file outputs for 1 input file
     elif len(sys.argv) == 2:
         print 'one file mode'
         filename = sys.argv[1]
         pad_file = PadFile(filename)
         convert(filename, samplerate=None, axis='xyzs', plot=True)
-    
+
     # batch files mode: only sound file output for each input file
     else:
         print 'batch files mode'
         for filename in sys.argv[1:]:
             pad_file = PadFile(filename)
             convert(filename)
-    
+
     sys.exit(0)
 
 if __name__ == "__main__":
