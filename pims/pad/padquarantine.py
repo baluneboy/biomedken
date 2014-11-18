c6e00d311d5d03fa12e1f1bf6cd65911f218460f
baluneboy@gmail.com <baluneboy@gmail.com@ab604922-b16d-4620-a341-c5f183439689>
2014-11-18 11:32:33 +0000

    Include amplitude checking in pad/padquarantine.py.
    git-svn-id: file:///Users/ken/Repository/biomedken/trunk@779 ab604922-b16d-4620-a341-c5f183439689

--- pims/pad/padquarantine.py
+++ pims/pad/padquarantine.py
@@ -7,9 +7,11 @@ import datetime
 import operator
 import shutil
 import subprocess
+import numpy as np
 from scipy import stats
 from pims.utils.pimsdateutil import datetime_to_ymd_path
 from pims.utils.pimsdateutil import pad_fullfilestr_to_start_stop
+from pims.files.utils import listdir_filename_pattern
 
 # get list of (file, rate) tuples sorted by rate
 def file_rate_tuples(r):
@@ -26,13 +28,14 @@ def grep_sample_rate(subdir):
     if not os.path.exists(subdir):
         raise OSError('%s does not exist' % subdir)
     cmd = 'grep SampleRate ' + os.path.join(subdir,'*.header')
-    process = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-    out, err = process.communicate()
+    process_header_files = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    out, err = process_header_files.communicate()
     splitout = out.split('\n')[:-1] # split on newlines & get rid of very last trailing newline
     return splitout
 
+# return True if bad time string; otherwise, False
 def bad_timestr(fullfilestr):
-    """grep to get file and sample rate in list"""
+    """return True if bad time string; otherwise, False"""
     d1, d2 = pad_fullfilestr_to_start_stop(fullfilestr)
     if not d1:
         print 'bad start part in %s' % fullfilestr
@@ -42,9 +45,39 @@ def bad_timestr(fullfilestr):
         return True
     return False
 
-# process single subdir to see if/what needs to be quarantined
-def process(subdir):
-    """process single subdir to see if/what needs to be quarantined"""
+# return 2d numpy array read from datafile input
+def array_fromfile(datafile, columns=4, out_dtype=np.float32):
+    """return 2d numpy array read from datafile input"""
+    with open(datafile, "rb") as f: 
+        a = np.fromfile(f, dtype=np.float32) # accel file: 32-bit float "singles"
+    b = np.reshape(a, (-1, columns))
+    if b.dtype == out_dtype:
+        return b
+    return b.astype(out_dtype)
+
+# max abs all data
+def max_abs(a):
+    """max abs all data"""
+    return np.max( np.abs(a[:,1:]) )
+
+# quarantine data and header file
+def quarantine(datafile):
+    """quarantine data and header file"""
+    dname = os.path.dirname(datafile)
+    bname = os.path.basename(datafile)
+    hdrfile = datafile + '.header'
+    
+    print 'quarantined %s' % os.path.basename( datafile )
+
+# check data file > 0.5 g
+def is_over_half_g(datafile):
+    """check data file > 0.5 g"""
+    a = array_fromfile(datafile, columns=4, out_dtype=np.float32)
+    return max_abs(a) > 0.5
+
+# process single subdir to see if/what needs to be quarantined, return count of quarantined
+def process_header_files(subdir):
+    """process single subdir to see if/what needs to be quarantined, return count of quarantined"""
     # get list of (file, rate) tuples sorted by rate
     r = grep_sample_rate(subdir)
     my_list = file_rate_tuples(r)
@@ -63,7 +96,7 @@ def process(subdir):
     
     # concat 2 lists
     quarantined_list = quarantined_list_fs + quarantined_list_bad_timestr
-    print 'length of quarantined list is %d for %s' % (len(quarantined_list), subdir)
+    #print 'length of quarantined list is %d for %s' % (len(quarantined_list), subdir)
         
     # if needed, then move to quarantined
     qdir = os.path.join(subdir, 'quarantined')
@@ -74,6 +107,25 @@ def process(subdir):
         #print 'move %s to %s' % (f.rstrip('.header'), qdir)
         shutil.move(f, qdir)                    # move header file
         shutil.move(f.rstrip('.header'), qdir)  # move data file
+    
+    return len(quarantined_list)
+
+# process single subdir to see if/what needs to be amplitude quarantined, return count of quarantined
+def process_amplitudes(subdir):
+    """process single subdir to see if/what needs to be amplitude quarantined, return count of quarantined"""
+    # get list of header files (this works for all sensors, that's why)
+    hdrfiles = listdir_filename_pattern(subdir, '.*\.header$')
+    quarantined_amplitude_list = [ i for i in hdrfiles if is_over_half_g(i.rstrip('.header')) ]
+    
+    # if needed, then move to quarantined
+    qdir = os.path.join(subdir, 'quarantined')
+    if quarantined_amplitude_list and not os.path.isdir(qdir):
+            os.mkdir( qdir )
+    for f in quarantined_amplitude_list:
+        shutil.move(f, qdir)                    # move header file
+        shutil.move(f.rstrip('.header'), qdir)  # move data file
+
+    return len(quarantined_amplitude_list)
 
 # iterate over day directory (only sams2 subdirs for now)
 def main(daydir):
@@ -83,7 +135,9 @@ def main(daydir):
     sams2_subdirs = [ i for i in subdirs if i.startswith('sams2_accel_') ]
     check_dirs = [ os.path.join(daydir, sd) for sd in sams2_subdirs ]
     for sams2dir in check_dirs:
-        process(sams2dir)
+        count1 = process_header_files(sams2dir)
+        count2 = process_amplitudes(sams2dir)
+        print 'length of quarantined list is %d + %d = %d for %s' % (count1, count2, count1 + count2, sams2dir)
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
